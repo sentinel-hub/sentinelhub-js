c3ba5cbd10d0afa6304b9f4195d21a448148de0e
Anze Skerlavaj <anze.skerlavaj@sinergise.com>
2020-02-13 13:23:29 +0100

    Move makeLayer() on end layers to avoid using TS magic

--- src/layer/Landsat7EOCloudLayer.ts
+++ src/layer/Landsat7EOCloudLayer.ts
@@ -3,4 +3,25 @@ import { AbstractSentinelHubV1OrV2WithCCLayer } from 'src/layer/AbstractSentinel
 
 export class Landsat7EOCloudLayer extends AbstractSentinelHubV1OrV2WithCCLayer {
   public readonly dataset = DATASET_EOCLOUD_LANDSAT7;
+
+  public static makeLayer(
+    layerInfo: any,
+    instanceId: string,
+    layerId: string,
+    evalscript: string | null,
+    evalscriptUrl: string | null,
+    title: string | null,
+    description: string | null,
+  ): Landsat7EOCloudLayer {
+    const maxCloudCoverPercent = layerInfo.settings.maxCC;
+    return new Landsat7EOCloudLayer(
+      instanceId,
+      layerId,
+      evalscript,
+      evalscriptUrl,
+      title,
+      description,
+      maxCloudCoverPercent,
+    );
+  }
 }
