82935f47a29386856b2958cf2f457ec28745f1d7
Anze <anze.skerlavaj@sinergise.com>
2020-02-13 16:27:06 +0100

    Setup jest to use TS, import correctly and run a first test on sentinelhub-js

---
+++ src/layer/__tests__/WmsLayer.ts
@@ -0,0 +1,20 @@
+import { BBox, CRS_EPSG4326, ApiType, MimeTypes, WmsLayer } from '../../';
+
+test('WmsLayer.getMapUrl returns an URL', () => {
+  const bbox = new BBox(CRS_EPSG4326, 19, 20, 20, 21);
+  const layer = new WmsLayer(
+    'https://proba-v-mep.esa.int/applications/geo-viewer/app/geoserver/ows',
+    'PROBAV_S1_TOA_333M',
+  );
+
+  const getMapParams = {
+    bbox: bbox,
+    fromTime: new Date(Date.UTC(2020, 1 - 1, 10, 0, 0, 0)), // 2020-01-10/2020-01-10
+    toTime: new Date(Date.UTC(2020, 1 - 1, 10, 23, 59, 59)),
+    width: 512,
+    height: 512,
+    format: MimeTypes.JPEG,
+  };
+  const imageUrl = layer.getMapUrl(getMapParams, ApiType.WMS);
+  expect(imageUrl).toBe('https://proba-v-mep.esa.int/applications/geo-viewer/app/geoserver/ows?version=1.1.1&service=WMS&request=GetMap&format=image%2Fjpeg&crs=EPSG%3A4326&layers=PROBAV_S1_TOA_333M&bbox=19%2C20%2C20%2C21&time=2020-01-10T00%3A00%3A00.000Z%2F2020-01-10T23%3A59%3A59.000Z&width=512&height=512&showlogo=false&transparent=true&');
+})
