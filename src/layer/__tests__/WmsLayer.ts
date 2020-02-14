0670d64ebaad00c42eb791c3f79803639d99e704
Anze <anze.skerlavaj@sinergise.com>
2020-02-14 10:29:40 +0100

    Allow checking URL origin and params

--- src/layer/__tests__/WmsLayer.ts
+++ src/layer/__tests__/WmsLayer.ts
@@ -1,10 +1,13 @@
+import "jest-setup";
+
 import { BBox, CRS_EPSG4326, ApiType, MimeTypes, WmsLayer } from 'src';
 
 test('WmsLayer.getMapUrl returns an URL', () => {
   const bbox = new BBox(CRS_EPSG4326, 19, 20, 20, 21);
+  const layerId = 'PROBAV_S1_TOA_333M';
   const layer = new WmsLayer(
     'https://proba-v-mep.esa.int/applications/geo-viewer/app/geoserver/ows',
-    'PROBAV_S1_TOA_333M',
+    layerId,
   );
 
   const getMapParams = {
@@ -19,5 +22,17 @@ test('WmsLayer.getMapUrl returns an URL', () => {
   expect(imageUrl).toBe(
     'https://proba-v-mep.esa.int/applications/geo-viewer/app/geoserver/ows?version=1.1.1&service=WMS&request=GetMap&format=image%2Fjpeg&crs=EPSG%3A4326&layers=PROBAV_S1_TOA_333M&bbox=19%2C20%2C20%2C21&time=2020-01-10T00%3A00%3A00.000Z%2F2020-01-10T23%3A59%3A59.000Z&width=512&height=512&showlogo=false&transparent=true&',
   );
-  expect(imageUrl).toIncludeQueryParam({ request: 'GetMap', crs: 'EPSG:4326' });
+  expect(imageUrl).toHaveOrigin('https://proba-v-mep.esa.int');
+  expect(imageUrl).toHaveQueryParams({
+    service: 'WMS',
+    version: '1.1.1',
+    request: 'GetMap',
+    format: 'image/jpeg',
+    layers: layerId,
+    crs: 'EPSG:4326',
+    bbox: '19,20,20,21',
+    time: '2020-01-10T00:00:00.000Z/2020-01-10T23:59:59.000Z',
+    width: '512',
+    height: '512',
+  });
 });
