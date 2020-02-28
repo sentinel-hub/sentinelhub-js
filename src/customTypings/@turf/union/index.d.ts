52342490000e6180b08426e7e31262d39e2e576b
Anze Skerlavaj <anze.skerlavaj@sinergise.com>
2020-02-28 09:32:45 +0100

    Remove custom typings for turf/union (not used anymore)

--- src/customTypings/@turf/union/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@turf/union' {
-  import { MultiPolygon, Polygon, Feature } from '@turf/helpers';
-  export default function union(
-    poly1: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon,
-    poly2: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon,
-  ): Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon;
-}
