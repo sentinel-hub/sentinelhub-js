a658e3eef714788d538a0f57bc2c4622dd974a08
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-03-02 14:02:33 +0100

    code review: datasets.ts: enums to strings for more consistency

--- src/layer/dataset.ts
+++ src/layer/dataset.ts
@@ -1,18 +1,3 @@
-enum ShDatasetParametersType {
-  BYOC = 'BYOC',
-  S1GRD = 'S1GRD',
-  S2 = 'S2',
-  S2GM = 'S2GM',
-  S3 = 'S3',
-  S3SLSTR = 'S3SLSTR',
-  S5PL2 = 'S5PL2',
-}
-
-enum EocDatasetParametersType {
-  S1GRD = 'S1GRD',
-  S5PL2 = 'S5PL2',
-}
-
 export type Dataset = {
   id: string;
   shJsonGetCapabilitiesDataset: string | null;
@@ -30,7 +15,7 @@ export const DATASET_AWSEU_S1GRD: Dataset = {
   shJsonGetCapabilitiesDataset: 'S1GRD',
   shWmsEvalsource: 'S1GRD',
   shProcessingApiDatasourceAbbreviation: 'S1GRD',
-  datasetParametersType: ShDatasetParametersType.S1GRD,
+  datasetParametersType: 'S1GRD',
   shServiceHostname: 'https://services.sentinel-hub.com/',
   searchIndexUrl: 'https://services.sentinel-hub.com/index/v3/collections/S1GRD/searchIndex',
   findDatesUrl: 'https://services.sentinel-hub.com/index/v3/collections/S1GRD/findAvailableData',
@@ -42,7 +27,7 @@ export const DATASET_EOCLOUD_S1GRD: Dataset = {
   shJsonGetCapabilitiesDataset: null,
   shWmsEvalsource: null, // it's complicated - allowed values are 'S1' (for IW) and 'S1_EW'
   shProcessingApiDatasourceAbbreviation: null,
-  datasetParametersType: EocDatasetParametersType.S1GRD,
+  datasetParametersType: 'S1GRD',
   shServiceHostname: 'https://eocloud.sentinel-hub.com/',
   searchIndexUrl: 'https://eocloud.sentinel-hub.com/index/s1/v1/search',
   findDatesUrl: null,
@@ -54,7 +39,7 @@ export const DATASET_S2L2A: Dataset = {
   shJsonGetCapabilitiesDataset: 'S2L2A',
   shWmsEvalsource: 'S2L2A',
   shProcessingApiDatasourceAbbreviation: 'S2L2A',
-  datasetParametersType: ShDatasetParametersType.S2,
+  datasetParametersType: 'S2',
   shServiceHostname: 'https://services.sentinel-hub.com/',
   searchIndexUrl: 'https://services.sentinel-hub.com/index/v3/collections/S2L2A/searchIndex',
   findDatesUrl: 'https://services.sentinel-hub.com/index/v3/collections/S2L2A/findAvailableData',
@@ -66,7 +51,7 @@ export const DATASET_S2L1C: Dataset = {
   shJsonGetCapabilitiesDataset: 'S2L1C',
   shWmsEvalsource: 'S2',
   shProcessingApiDatasourceAbbreviation: 'S2L1C',
-  datasetParametersType: ShDatasetParametersType.S2,
+  datasetParametersType: 'S2',
   shServiceHostname: 'https://services.sentinel-hub.com/',
   searchIndexUrl: 'https://services.sentinel-hub.com/index/v3/collections/S2L1C/searchIndex',
   findDatesUrl: 'https://services.sentinel-hub.com/index/v3/collections/S2L1C/findAvailableData',
@@ -78,7 +63,7 @@ export const DATASET_S3SLSTR: Dataset = {
   shJsonGetCapabilitiesDataset: 'S3SLSTR',
   shWmsEvalsource: 'S3SLSTR',
   shProcessingApiDatasourceAbbreviation: 'S3SLSTR',
-  datasetParametersType: ShDatasetParametersType.S3SLSTR,
+  datasetParametersType: 'S3SLSTR',
   shServiceHostname: 'https://creodias.sentinel-hub.com/',
   searchIndexUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S3SLSTR/searchIndex',
   findDatesUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S3SLSTR/findAvailableData',
@@ -90,7 +75,7 @@ export const DATASET_S3OLCI: Dataset = {
   shJsonGetCapabilitiesDataset: 'S3OLCI',
   shWmsEvalsource: 'S3OLCI',
   shProcessingApiDatasourceAbbreviation: 'S3OLCI',
-  datasetParametersType: ShDatasetParametersType.S3,
+  datasetParametersType: 'S3',
   shServiceHostname: 'https://creodias.sentinel-hub.com/',
   searchIndexUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S3OLCI/searchIndex',
   findDatesUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S3OLCI/findAvailableData',
@@ -102,7 +87,7 @@ export const DATASET_S5PL2: Dataset = {
   shJsonGetCapabilitiesDataset: 'S5PL2',
   shWmsEvalsource: 'S5P_L2',
   shProcessingApiDatasourceAbbreviation: 'S5PL2',
-  datasetParametersType: ShDatasetParametersType.S5PL2,
+  datasetParametersType: 'S5PL2',
   shServiceHostname: 'https://creodias.sentinel-hub.com/',
   searchIndexUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S5PL2/searchIndex',
   findDatesUrl: 'https://creodias.sentinel-hub.com/index/v3/collections/S5PL2/findAvailableData',
@@ -198,9 +183,9 @@ export const DATASET_BYOC: Dataset = {
   shJsonGetCapabilitiesDataset: 'CUSTOM',
   shWmsEvalsource: 'CUSTOM',
   shProcessingApiDatasourceAbbreviation: 'CUSTOM',
-  datasetParametersType: null,
+  datasetParametersType: 'BYOC',
   shServiceHostname: 'https://services.sentinel-hub.com/',
   searchIndexUrl: 'https://services.sentinel-hub.com/byoc/v3/collections/CUSTOM/searchIndex',
-  orbitTimeMinutes: null,
   findDatesUrl: null,
+  orbitTimeMinutes: null,
 };
