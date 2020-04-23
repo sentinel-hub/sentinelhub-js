7492921285376dc431ed0824792b1ed98c21fa5c
Ignasi Espinosa <ignasi.espinosa@sinergise.com>
2020-04-23 11:05:39 +0200

    byoc dataset added on dataset_from_json_getcapabilities

--- src/layer/LayersFactory.ts
+++ src/layer/LayersFactory.ts
@@ -59,6 +59,7 @@ export class LayersFactory {
     [DATASET_EOCLOUD_ENVISAT_MERIS.shJsonGetCapabilitiesDataset]: DATASET_EOCLOUD_ENVISAT_MERIS,
     [DATASET_MODIS.shJsonGetCapabilitiesDataset]: DATASET_MODIS,
     [DATASET_AWS_DEM.shJsonGetCapabilitiesDataset]: DATASET_AWS_DEM,
+    [DATASET_BYOC.shJsonGetCapabilitiesDataset]:DATASET_BYOC
   };
 
   private static readonly DATASET_FROM_JSON_GETCAPABILITIES_V1: Record<string, Dataset> = {
@@ -123,9 +124,7 @@ export class LayersFactory {
       dataset:
         layerInfo.dataset && LayersFactory.DATASET_FROM_JSON_GETCAPAPABILITIES[layerInfo.dataset]
           ? LayersFactory.DATASET_FROM_JSON_GETCAPAPABILITIES[layerInfo.dataset]
-          : layerInfo.dataset === 'CUSTOM' 
-            ? {id:'CUSTOM'} 
-            : null,
+          : null,
     }));
 
     const filteredLayersInfos =
