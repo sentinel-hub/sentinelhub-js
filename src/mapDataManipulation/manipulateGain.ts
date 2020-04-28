7a252f2dbceba90ee9b2504769ee19adfbd95259
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-04-28 18:44:46 +0200

    cleanup of manipulateGain

--- src/mapDataManipulation/manipulateGain.ts
+++ src/mapDataManipulation/manipulateGain.ts
@@ -1,5 +1,11 @@
 import { mapDataManipulation } from './mapDataManipulation';
 
+// This is the algorithm closest to the algorithm on the backend.
+// The algorithm works with numbers between 0 and 1, so we must:
+// - change the values from [0, 255] to [0, 1]
+// - change the values according to the algorithm
+// - change the values from [0, 1] back to [0, 255]
+
 export async function manipulateGain(
   originalBlob: Blob,
   newGain: number,
@@ -18,53 +24,23 @@ export async function manipulateGain(
   let newGreenValues = [...Array(256).keys()];
   let newBlueValues = [...Array(256).keys()];
 
-  console.log('manipulateGain start', { minValue, maxValue, gain, factor, offset, newRedValues });
-
+  // change the values from [0, 255] to [0, 1]
   newRedValues = newRedValues.map(x => (1 / 255) * x);
   newGreenValues = newGreenValues.map(x => (1 / 255) * x);
   newBlueValues = newBlueValues.map(x => (1 / 255) * x);
 
-  console.log('manipulateGain first step', { newRedValues, newGreenValues, newBlueValues });
+  // change the values according to the algorithm
+  newRedValues = newRedValues.map(x => Math.max(0.0, x * factor + offset));
+  newGreenValues = newGreenValues.map(x => Math.max(0.0, x * factor + offset));
+  newBlueValues = newBlueValues.map(x => Math.max(0.0, x * factor + offset));
 
-  for (let i in newRedValues) {
-    newRedValues[i] = Math.max(0.0, newRedValues[i] * factor + offset);
-    newGreenValues[i] = Math.max(0.0, newGreenValues[i] * factor + offset);
-    newBlueValues[i] = Math.max(0.0, newBlueValues[i] * factor + offset);
-  }
-
-  console.log('manipulateGain second step', { newRedValues, newGreenValues, newBlueValues });
-
+  // change the values from [0, 1] back to [0, 255]
   newRedValues = newRedValues.map(x => Math.round((255 / 1) * x));
   newGreenValues = newGreenValues.map(x => Math.round((255 / 1) * x));
   newBlueValues = newBlueValues.map(x => Math.round((255 / 1) * x));
 
-  console.log('manipulateGain final', { newRedValues, newGreenValues, newBlueValues });
-
-  // params.saveManipulationFunctionToStore('redGraph', function (el) {
-  //   return newRedValues[el];
-  // });
-  // params.saveManipulationFunctionToStore('greenGraph', function (el) {
-  //   return newGreenValues[el];
-  // });
-  // params.saveManipulationFunctionToStore('blueGraph', function (el) {
-  //   return newBlueValues[el];
-  // });
-
-  // call mapDataManipulation
-
-  // const manipulatePixel = function (r, g, b, a) {
-  //   r = manFuns && manFuns.redFun ? manFuns.redFun(r) : r;
-  //   g = manFuns && manFuns.greenFun ? manFuns.greenFun(g) : g;
-  //   b = manFuns && manFuns.blueFun ? manFuns.blueFun(b) : b;
-  //   a = manFuns && manFuns.alphaFun ? manFuns.alphaFun(a) : a;
-  //   return { r, g, b, a };
-  // };
-
   const manipulatePixel = function(r: number, g: number, b: number, a: number) {
     return { r: newRedValues[r], g: newGreenValues[g], b: newBlueValues[b], a };
   };
-
-  const newBlob = await mapDataManipulation(originalBlob, manipulatePixel);
-
-  return newBlob;
+  return await mapDataManipulation(originalBlob, manipulatePixel);
 }
