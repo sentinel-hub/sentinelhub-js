845bf6613745f1bf711a5842c1e77024763909fe
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-04-28 20:33:16 +0200

    better comment of the manipulateGain algorithm

--- src/mapDataManipulation/manipulateGain.ts
+++ src/mapDataManipulation/manipulateGain.ts
@@ -2,9 +2,9 @@ import { mapDataManipulation } from './mapDataManipulation';
 
 // This is the algorithm closest to the algorithm on the backend.
 // The algorithm works with numbers between 0 and 1, so we must:
-// - change the values from [0, 255] to [0, 1]
+// - change the interval of the values from [0, 255] to [0, 1]
 // - change the values according to the algorithm
-// - change the values from [0, 1] back to [0, 255]
+// - change the interval of the values from [0, 1] back to [0, 255]
 
 export async function manipulateGain(
   originalBlob: Blob,
@@ -24,7 +24,7 @@ export async function manipulateGain(
   let newGreenValues = [...Array(256).keys()];
   let newBlueValues = [...Array(256).keys()];
 
-  // change the values from [0, 255] to [0, 1]
+  // change the interval of the values from [0, 255] to [0, 1]
   newRedValues = newRedValues.map(x => (1 / 255) * x);
   newGreenValues = newGreenValues.map(x => (1 / 255) * x);
   newBlueValues = newBlueValues.map(x => (1 / 255) * x);
@@ -34,7 +34,7 @@ export async function manipulateGain(
   newGreenValues = newGreenValues.map(x => Math.max(0.0, x * factor + offset));
   newBlueValues = newBlueValues.map(x => Math.max(0.0, x * factor + offset));
 
-  // change the values from [0, 1] back to [0, 255]
+  // change the interval of the values from [0, 1] back to [0, 255]
   newRedValues = newRedValues.map(x => Math.round((255 / 1) * x));
   newGreenValues = newGreenValues.map(x => Math.round((255 / 1) * x));
   newBlueValues = newBlueValues.map(x => Math.round((255 / 1) * x));
