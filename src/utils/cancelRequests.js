4a98fb26dd9b3fa3c45ac520d3df2b25ec60da20
Ignasi Espinosa <ignasi.espinosa@sinergise.com>
2020-04-30 14:38:12 +0200

    cancelRequests typing

--- src/utils/cancelRequests.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const cancelFactory = () => {
-  return axios.CancelToken.source();
-};
-
-export const isCancelled = err => {
-  return axios.isCancel(err);
-};
---
+++ src/utils/cancelRequests.ts
@@ -0,0 +1,9 @@
+import axios, { CancelTokenSource } from 'axios';
+
+export const cancelFactory = (): CancelTokenSource => {
+  return axios.CancelToken.source();
+};
+
+export const isCancelled = (err:Error): boolean => {
+  return axios.isCancel(err);
+};
