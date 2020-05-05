8532c19da6ac825fcac02d14beb9276feb54a41a
Anze <anze.skerlavaj@sinergise.com>
2020-05-05 15:24:13 +0200

    Refactor: mMake AbstractSentinelHubV3WithCCLayer deal with maxcc parameters for Processing API

--- src/layer/S2L1CLayer.ts
+++ src/layer/S2L1CLayer.ts
@@ -1,12 +1,6 @@
 import { DATASET_S2L1C } from 'src/layer/dataset';
 import { AbstractSentinelHubV3WithCCLayer } from './AbstractSentinelHubV3WithCCLayer';
-import { ProcessingPayload } from 'src/layer/processing';
 
 export class S2L1CLayer extends AbstractSentinelHubV3WithCCLayer {
   public readonly dataset = DATASET_S2L1C;
-
-  protected async updateProcessingGetMapPayload(payload: ProcessingPayload): Promise<ProcessingPayload> {
-    payload.input.data[0].dataFilter.maxCloudCoverage = this.maxCloudCoverPercent;
-    return payload;
-  }
 }
