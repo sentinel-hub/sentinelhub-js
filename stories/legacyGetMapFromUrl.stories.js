6f8dae70aae637b5fe1e0dc512c98c99d20372a5
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-05-07 17:44:59 +0200

    add stories for legacyGetMapFromUrl

---
+++ stories/legacyGetMapFromUrl.stories.js
@@ -0,0 +1,296 @@
+import { setAuthTokenWithOAuthCredentials } from './storiesUtils';
+
+import { legacyGetMapFromUrl, ApiType } from '../dist/sentinelHub.esm';
+
+if (!process.env.INSTANCE_ID) {
+  throw new Error('INSTANCE_ID environment variable is not defined!');
+}
+
+if (!process.env.S2L2A_LAYER_ID) {
+  throw new Error('S2L2A_LAYER_ID environment variable is not defined!');
+}
+
+const instanceId = process.env.INSTANCE_ID;
+const s2l2aLayerId = process.env.S2L2A_LAYER_ID;
+
+const gain = 2;
+const gamma = 2;
+
+export default {
+  title: 'legacyGetMapFromUrl',
+};
+
+const baseUrl = `https://services.sentinel-hub.com/ogc/wms/${instanceId}`;
+
+const queryParams = `version=1.1.1&service=WMS&request=GetMap&format=image%2Fjpeg&crs=EPSG%3A3857&layers=${s2l2aLayerId}&bbox=-430493.3433021127%2C4931105.568733289%2C-410925.4640611076%2C4950673.447974297&time=2019-07-01T00%3A00%3A00.000Z%2F2020-01-15T23%3A59%3A59.000Z&width=512&height=512&showlogo=false&transparent=true&maxcc=20&preview=3&bgcolor=000000`;
+const queryParamsGain = queryParams + `&gain=${gain}`;
+const queryParamsGamma = queryParams + `&gamma=${gamma}`;
+const queryParamsGainGamma = queryParams + `&gain=${gain}` + `&gamma=${gamma}`;
+
+// Processing API
+
+export const ProcessingLegacyGetMapFromUrl = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for Processing API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+
+  const img = document.createElement('img');
+  img.width = '512';
+  img.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>Processing LegacyGetMapFromUrl With WMS Fallback</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', img);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    const imageBlob = await legacyGetMapFromUrl(`${baseUrl}?${queryParams}`, ApiType.PROCESSING, true);
+    img.src = URL.createObjectURL(imageBlob);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const ProcessingLegacyGetMapFromUrlWithGain = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for Processing API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+
+  const imgGainIs1 = document.createElement('img');
+  imgGainIs1.width = '512';
+  imgGainIs1.height = '512';
+
+  const imgGainIs2 = document.createElement('img');
+  imgGainIs2.width = '512';
+  imgGainIs2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>Processing LegacyGetMapFromUrl With WMS Fallback; no gain vs gain=${gain}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs1);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    try {
+      const imageBlobGainIs1 = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParams}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgGainIs1.src = URL.createObjectURL(imageBlobGainIs1);
+
+      const imageBlobGainIs2 = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParamsGain}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgGainIs2.src = URL.createObjectURL(imageBlobGainIs2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const ProcessingLegacyGetMapFromUrlWithGamma = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for Processing API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+
+  const imgGammaIs1 = document.createElement('img');
+  imgGammaIs1.width = '512';
+  imgGammaIs1.height = '512';
+
+  const imgGammaIs2 = document.createElement('img');
+  imgGammaIs2.width = '512';
+  imgGammaIs2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>Processing LegacyGetMapFromUrl With WMS Fallback; no gamma vs gamma=${gamma}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs1);
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs2);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    try {
+      const imageBlobGammaIs1 = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParams}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgGammaIs1.src = URL.createObjectURL(imageBlobGammaIs1);
+
+      const imageBlobGammaIs2 = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParamsGamma}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgGammaIs2.src = URL.createObjectURL(imageBlobGammaIs2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const ProcessingLegacyGetMapFromUrlWithGainAndGamma = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for Processing API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+
+  const imgNoGainGamma = document.createElement('img');
+  imgNoGainGamma.width = '512';
+  imgNoGainGamma.height = '512';
+
+  const imgGainGammaAre2 = document.createElement('img');
+  imgGainGammaAre2.width = '512';
+  imgGainGammaAre2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>Processing LegacyGetMapFromUrl With WMS Fallback; no gain/gamma vs gain=${gain} and gamma=${gamma}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainGammaAre2);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    try {
+      const imageBlobNoGainGamma = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParams}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgNoGainGamma.src = URL.createObjectURL(imageBlobNoGainGamma);
+
+      const imageBlobGainGamaAre2 = await legacyGetMapFromUrl(
+        `${baseUrl}?${queryParamsGainGamma}`,
+        ApiType.PROCESSING,
+        true,
+      );
+      imgGainGammaAre2.src = URL.createObjectURL(imageBlobGainGamaAre2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+// OGC API
+
+export const WMSLegacyGetMapFromUrl = () => {
+  const img = document.createElement('img');
+  img.width = '512';
+  img.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>WMS LegacyGetMapFromUrl</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', img);
+
+  const perform = async () => {
+    const imageBlob = await legacyGetMapFromUrl(`${baseUrl}?${queryParams}`);
+    img.src = URL.createObjectURL(imageBlob);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const WMSLegacyGetMapFromUrlWithGain = () => {
+  const imgGainIs1 = document.createElement('img');
+  imgGainIs1.width = '512';
+  imgGainIs1.height = '512';
+
+  const imgGainIs2 = document.createElement('img');
+  imgGainIs2.width = '512';
+  imgGainIs2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>WMS LegacyGetMapFromUrl; no gain vs gain=${gain}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs1);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
+
+  const perform = async () => {
+    try {
+      const imageBlobGainIs1 = await legacyGetMapFromUrl(`${baseUrl}?${queryParams}`);
+      imgGainIs1.src = URL.createObjectURL(imageBlobGainIs1);
+
+      const imageBlobGainIs2 = await legacyGetMapFromUrl(`${baseUrl}?${queryParamsGain}`);
+      imgGainIs2.src = URL.createObjectURL(imageBlobGainIs2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const WMSLegacyGetMapFromUrlWithGamma = () => {
+  const imgGammaIs1 = document.createElement('img');
+  imgGammaIs1.width = '512';
+  imgGammaIs1.height = '512';
+
+  const imgGammaIs2 = document.createElement('img');
+  imgGammaIs2.width = '512';
+  imgGammaIs2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>WMS LegacyGetMapFromUrl; no gamma vs gamma=${gamma}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs1);
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs2);
+
+  const perform = async () => {
+    try {
+      const imageBlobGammaIs1 = await legacyGetMapFromUrl(`${baseUrl}?${queryParams}`);
+      imgGammaIs1.src = URL.createObjectURL(imageBlobGammaIs1);
+
+      const imageBlobGammaIs2 = await legacyGetMapFromUrl(`${baseUrl}?${queryParamsGamma}`);
+      imgGammaIs2.src = URL.createObjectURL(imageBlobGammaIs2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const WMSLegacyGetMapFromUrlWithGainAndGamma = () => {
+  const imgNoGainGamma = document.createElement('img');
+  imgNoGainGamma.width = '512';
+  imgNoGainGamma.height = '512';
+
+  const imgGainGammaAre2 = document.createElement('img');
+  imgGainGammaAre2.width = '512';
+  imgGainGammaAre2.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = `<h2>WMS LegacyGetMapFromUrl; no gain/gamma vs gain=${gain} and gamma=${gamma}</h2>`;
+  wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainGammaAre2);
+
+  const perform = async () => {
+    try {
+      const imageBlobNoGainGamma = await legacyGetMapFromUrl(`${baseUrl}?${queryParams}`);
+      imgNoGainGamma.src = URL.createObjectURL(imageBlobNoGainGamma);
+
+      const imageBlobGainGamaAre2 = await legacyGetMapFromUrl(`${baseUrl}?${queryParamsGainGamma}`);
+      imgGainGammaAre2.src = URL.createObjectURL(imageBlobGainGamaAre2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
