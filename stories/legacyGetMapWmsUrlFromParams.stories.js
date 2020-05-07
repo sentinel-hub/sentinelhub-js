9a3bc636ad6f610debc5b6252fa5133ee80c2dda
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-05-07 23:59:54 +0200

    enable gain, gamma for legacy

--- stories/legacyGetMapWmsUrlFromParams.stories.js
+++ stories/legacyGetMapWmsUrlFromParams.stories.js
@@ -13,7 +13,7 @@ const s2l2aLayerId = process.env.S2L2A_LAYER_ID;
 const baseUrl = `https://services.sentinel-hub.com/ogc/wms/${instanceId}`;
 
 export default {
-  title: "legacyGetMapWmsUrlFromParams [DOESN'T SUPPORT GAIN, GAMMA]",
+  title: 'legacyGetMapWmsUrlFromParams',
 };
 
 const madridBboxAsArrayEPSG3857 = [
@@ -94,6 +94,7 @@ export const legacyGetMapWmsUrlFromParamsGainGamma = () => {
 
   const wrapperEl = document.createElement('div');
   wrapperEl.innerHTML = '<h2>WMS LegacyGetMapFromUrl</h2>';
+  wrapperEl.innerHTML = "<h3>[DOESN'T SUPPORT GAIN/GAMMA so an error should be shown]</h3>";
   wrapperEl.innerHTML += '<h4>no gain/gamma | gain | gamma | gain and gamma</h4>';
   wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
   wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
