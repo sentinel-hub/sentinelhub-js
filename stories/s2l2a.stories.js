883b12a76b0d03262122d7183255555f70da54a2
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-05-07 19:11:47 +0200

    refactor S2L2A gain,gamma stories

--- stories/s2l2a.stories.js
+++ stories/s2l2a.stories.js
@@ -40,6 +40,9 @@ const geometryMultiPolygon = {
   ],
 };
 
+const gain = 2;
+const gamma = 2;
+
 export default {
   title: 'Sentinel 2 L2A',
 };
@@ -98,60 +101,31 @@ export const GetMapWMS = () => {
   return wrapperEl;
 };
 
-export const GetMapURLGain = () => {
-  const imgGainIs1 = document.createElement('img');
-  imgGainIs1.width = '512';
-  imgGainIs1.height = '512';
+export const getMapURLGainGamma = () => {
+  const imgNoGainGamma = document.createElement('img');
+  imgNoGainGamma.width = '256';
+  imgNoGainGamma.height = '256';
 
   const imgGainIs2 = document.createElement('img');
-  imgGainIs2.width = '512';
-  imgGainIs2.height = '512';
+  imgGainIs2.width = '256';
+  imgGainIs2.height = '256';
+
+  const imgGammaIs2 = document.createElement('img');
+  imgGammaIs2.width = '256';
+  imgGammaIs2.height = '256';
+
+  const imgGainGammaAre2 = document.createElement('img');
+  imgGainGammaAre2.width = '256';
+  imgGainGammaAre2.height = '256';
 
   const wrapperEl = document.createElement('div');
-  wrapperEl.innerHTML = '<h2>GetMapUrl (WMS) for Sentinel-2 L2A; gain</h2>';
-  wrapperEl.insertAdjacentElement('beforeend', imgGainIs1);
+  wrapperEl.innerHTML = '<h2>S2L2A getMapURLGainGamma</h2>';
+  wrapperEl.innerHTML += '<h4>no gain/gamma | gain | gamma | gain and gamma</h4>';
+  wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
   wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainGammaAre2);
 
-  const layerS2L2A = new S2L2ALayer({ instanceId, layerId, maxCloudCoverPercent: 0 });
-
-  const getMapParams = {
-    bbox: bbox4326,
-    fromTime: new Date(Date.UTC(2018, 9 - 1, 22, 0, 0, 0)),
-    toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
-    width: 512,
-    height: 512,
-    format: MimeTypes.JPEG,
-  };
-
-  const imageUrl1 = layerS2L2A.getMapUrl(getMapParams, ApiType.WMS);
-  imgGainIs1.src = imageUrl1;
-
-  const getMapParamsWithGainIs2 = {
-    ...getMapParams,
-    gain: 2,
-  };
-
-  const imageUrl2 = layerS2L2A.getMapUrl(getMapParamsWithGainIs2, ApiType.WMS);
-  imgGainIs2.src = imageUrl2;
-
-  return wrapperEl;
-};
-
-export const GetMapWMSGain = () => {
-  const imgGainIs1 = document.createElement('img');
-  imgGainIs1.width = '512';
-  imgGainIs1.height = '512';
-
-  const imgGainIs2 = document.createElement('img');
-  imgGainIs2.width = '512';
-  imgGainIs2.height = '512';
-
-  const wrapperEl = document.createElement('div');
-  wrapperEl.innerHTML = '<h2>GetMap with WMS for Sentinel-2 L2A; gain</h2>';
-  wrapperEl.insertAdjacentElement('beforeend', imgGainIs1);
-  wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
-
-  // getMap is async:
   const perform = async () => {
     const layerS2L2A = new S2L2ALayer({ instanceId, layerId, maxCloudCoverPercent: 0 });
 
@@ -163,15 +137,88 @@ export const GetMapWMSGain = () => {
       height: 512,
       format: MimeTypes.JPEG,
     };
-    const imageBlobGainIs1 = await layerS2L2A.getMap(getMapParams, ApiType.WMS);
-    imgGainIs1.src = URL.createObjectURL(imageBlobGainIs1);
 
-    const getMapParamsWithGainIs2 = {
-      ...getMapParams,
-      gain: 2,
+    const getMapParamsGainIs2 = { ...getMapParams, gain: gain };
+    const getMapParamsGammaIs2 = { ...getMapParams, gamma: gamma };
+    const getMapParamsGainGammaAre2 = { ...getMapParams, gain: gain, gamma: gamma };
+
+    try {
+      const imageBlobNoGainGamma = await layerS2L2A.getMapUrl(getMapParams, ApiType.WMS);
+      imgNoGainGamma.src = imageBlobNoGainGamma;
+
+      const imageBlobGainIs2 = await layerS2L2A.getMapUrl(getMapParamsGainIs2, ApiType.WMS);
+      imgGainIs2.src = imageBlobGainIs2;
+
+      const imageBlobGammaIs2 = await layerS2L2A.getMapUrl(getMapParamsGammaIs2, ApiType.WMS);
+      imgGammaIs2.src = imageBlobGammaIs2;
+
+      const imageBlobGainGamaAre2 = await layerS2L2A.getMapUrl(getMapParamsGainGammaAre2, ApiType.WMS);
+      imgGainGammaAre2.src = imageBlobGainGamaAre2;
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const getMapWMSGainGamma = () => {
+  const imgNoGainGamma = document.createElement('img');
+  imgNoGainGamma.width = '256';
+  imgNoGainGamma.height = '256';
+
+  const imgGainIs2 = document.createElement('img');
+  imgGainIs2.width = '256';
+  imgGainIs2.height = '256';
+
+  const imgGammaIs2 = document.createElement('img');
+  imgGammaIs2.width = '256';
+  imgGammaIs2.height = '256';
+
+  const imgGainGammaAre2 = document.createElement('img');
+  imgGainGammaAre2.width = '256';
+  imgGainGammaAre2.height = '256';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>S2L2A getMapWMSGainGamma</h2>';
+  wrapperEl.innerHTML += '<h4>no gain/gamma | gain | gamma | gain and gamma</h4>';
+  wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainGammaAre2);
+
+  const perform = async () => {
+    const layerS2L2A = new S2L2ALayer({ instanceId, layerId, maxCloudCoverPercent: 0 });
+
+    const getMapParams = {
+      bbox: bbox4326,
+      fromTime: new Date(Date.UTC(2018, 9 - 1, 22, 0, 0, 0)),
+      toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
+      width: 512,
+      height: 512,
+      format: MimeTypes.JPEG,
     };
-    const imageBlobGainIs2 = await layerS2L2A.getMap(getMapParamsWithGainIs2, ApiType.WMS);
-    imgGainIs2.src = URL.createObjectURL(imageBlobGainIs2);
+
+    const getMapParamsGainIs2 = { ...getMapParams, gain: gain };
+    const getMapParamsGammaIs2 = { ...getMapParams, gamma: gamma };
+    const getMapParamsGainGammaAre2 = { ...getMapParams, gain: gain, gamma: gamma };
+
+    try {
+      const imageBlobNoGainGamma = await layerS2L2A.getMap(getMapParams, ApiType.WMS);
+      imgNoGainGamma.src = URL.createObjectURL(imageBlobNoGainGamma);
+
+      const imageBlobGainIs2 = await layerS2L2A.getMap(getMapParamsGainIs2, ApiType.WMS);
+      imgGainIs2.src = URL.createObjectURL(imageBlobGainIs2);
+
+      const imageBlobGammaIs2 = await layerS2L2A.getMap(getMapParamsGammaIs2, ApiType.WMS);
+      imgGammaIs2.src = URL.createObjectURL(imageBlobGammaIs2);
+
+      const imageBlobGainGamaAre2 = await layerS2L2A.getMap(getMapParamsGainGammaAre2, ApiType.WMS);
+      imgGainGammaAre2.src = URL.createObjectURL(imageBlobGainGamaAre2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
   };
   perform().then(() => {});
 
@@ -253,6 +300,87 @@ export const GetMapProcessing = () => {
     };
     const imageBlob = await layerS2L2A.getMap(getMapParams, ApiType.PROCESSING);
     img.src = URL.createObjectURL(imageBlob);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const getMapProcessingGainGamma = () => {
+  const imgNoGainGamma = document.createElement('img');
+  imgNoGainGamma.width = '256';
+  imgNoGainGamma.height = '256';
+
+  const imgGainIs2 = document.createElement('img');
+  imgGainIs2.width = '256';
+  imgGainIs2.height = '256';
+
+  const imgGammaIs2 = document.createElement('img');
+  imgGammaIs2.width = '256';
+  imgGammaIs2.height = '256';
+
+  const imgGainGammaAre2 = document.createElement('img');
+  imgGainGammaAre2.width = '256';
+  imgGainGammaAre2.height = '256';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>S2L2A getMapProcessingGainGamma</h2>';
+  wrapperEl.innerHTML += '<h4>no gain/gamma | gain | gamma | gain and gamma</h4>';
+  wrapperEl.insertAdjacentElement('beforeend', imgNoGainGamma);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGammaIs2);
+  wrapperEl.insertAdjacentElement('beforeend', imgGainGammaAre2);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    const layerS2L2A = new S2L2ALayer({
+      instanceId,
+      layerId,
+      maxCloudCoverPercent: 0,
+      evalscript: `
+      //VERSION=3
+      function setup() {
+        return {
+          input: ["B02", "B03", "B04"],
+          output: { bands: 3 }
+        };
+      }
+
+      function evaluatePixel(sample) {
+        return [2.5 * sample.B04, 2.5 * sample.B03, 2.5 * sample.B02];
+      }
+    `,
+    });
+
+    const getMapParams = {
+      bbox: bbox4326,
+      fromTime: new Date(Date.UTC(2018, 9 - 1, 22, 0, 0, 0)),
+      toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
+      width: 512,
+      height: 512,
+      format: MimeTypes.JPEG,
+    };
+
+    const getMapParamsGainIs2 = { ...getMapParams, gain: gain };
+    const getMapParamsGammaIs2 = { ...getMapParams, gamma: gamma };
+    const getMapParamsGainGammaAre2 = { ...getMapParams, gain: gain, gamma: gamma };
+
+    try {
+      const imageBlobNoGainGamma = await layerS2L2A.getMap(getMapParams, ApiType.PROCESSING);
+      imgNoGainGamma.src = URL.createObjectURL(imageBlobNoGainGamma);
+
+      const imageBlobGainIs2 = await layerS2L2A.getMap(getMapParamsGainIs2, ApiType.PROCESSING);
+      imgGainIs2.src = URL.createObjectURL(imageBlobGainIs2);
+
+      const imageBlobGammaIs2 = await layerS2L2A.getMap(getMapParamsGammaIs2, ApiType.PROCESSING);
+      imgGammaIs2.src = URL.createObjectURL(imageBlobGammaIs2);
+
+      const imageBlobGainGamaAre2 = await layerS2L2A.getMap(getMapParamsGainGammaAre2, ApiType.PROCESSING);
+      imgGainGammaAre2.src = URL.createObjectURL(imageBlobGainGamaAre2);
+    } catch (err) {
+      wrapperEl.innerHTML += '<pre>ERROR OCCURED: ' + err + '</pre>';
+    }
   };
   perform().then(() => {});
 
