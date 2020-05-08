c1b4e75e7af814348635409bfaaf30833851ad82
jjezersek <jan.jezersek@sinergise.com>
2020-05-08 14:34:49 +0200

    Added extra tile parameters for Sentinel-1 and Sentinel-2

--- src/layer/S1GRDEOCloudLayer.ts
+++ src/layer/S1GRDEOCloudLayer.ts
@@ -2,7 +2,7 @@ import { DATASET_EOCLOUD_S1GRD } from 'src/layer/dataset';
 
 import { AbstractSentinelHubV1OrV2Layer } from 'src/layer/AbstractSentinelHubV1OrV2Layer';
 import { AcquisitionMode, Polarization } from 'src/layer/S1GRDAWSEULayer';
-import { OrbitDirection, MosaickingOrder } from 'src/layer/const';
+import { OrbitDirection, MosaickingOrder, Link } from 'src/layer/const';
 
 /*
   Note: the usual combinations are IW + DV/SV + HIGH and EW + DH/SH + MEDIUM.
@@ -130,5 +130,18 @@ export class S1GRDEOCloudLayer extends AbstractSentinelHubV1OrV2Layer {
       result.orbitDirection = this.orbitDirection;
     }
     return result;
+  }
+
+  protected getTileLinks(tile: Record<string, any>): Link[] {
+    return [{
+      href: tile.pathFragment,
+      rel:'self',
+      title: 'EOCloudPath'
+    },
+    {
+      href: `https://finder.creodias.eu/files/${tile.pathFragment.split('eodata')[1]}/preview/quick-look.png`,
+      rel:'self',
+      title: 'Preview'
+    }];
   }
 }
--- src/layer/S2L2ALayer.ts
+++ src/layer/S2L2ALayer.ts
@@ -1,6 +1,7 @@
 import { DATASET_S2L2A } from 'src/layer/dataset';
 import { AbstractSentinelHubV3WithCCLayer } from './AbstractSentinelHubV3WithCCLayer';
 import { ProcessingPayload } from 'src/layer/processing';
+import { Link } from 'src/layer/const';
 
 export class S2L2ALayer extends AbstractSentinelHubV3WithCCLayer {
   public readonly dataset = DATASET_S2L2A;
@@ -8,5 +9,13 @@ export class S2L2ALayer extends AbstractSentinelHubV3WithCCLayer {
   protected async updateProcessingGetMapPayload(payload: ProcessingPayload): Promise<ProcessingPayload> {
     payload.input.data[0].dataFilter.maxCloudCoverage = this.maxCloudCoverPercent;
     return payload;
+  }
+
+  protected getTileLinks(tile: Record<string, any>): Link[] {
+    return [{
+      href: tile.dataUri,
+      rel:'self',
+      title: 'AWSPath'
+    }];
   }
 }
