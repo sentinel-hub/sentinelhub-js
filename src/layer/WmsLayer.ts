d95ebda4b85f8e9a3e496b63526b1d4c27aaa34d
shooshte <miha.sustersic.work@gmail.com>
2020-06-09 13:55:57 +0200

    added ensureTimeout

--- src/layer/WmsLayer.ts
+++ src/layer/WmsLayer.ts
@@ -6,6 +6,7 @@ import { wmsGetMapUrl } from 'src/layer/wms';
 import { AbstractLayer } from 'src/layer/AbstractLayer';
 import { fetchGetCapabilitiesXml } from './utils';
 import { RequestConfiguration } from 'src/utils/cancelRequests';
+import { ensureTimeout } from 'src/utils/ensureTimeout';
 
 interface ConstructorParameters {
   baseUrl?: string;
@@ -51,81 +52,86 @@ export class WmsLayer extends AbstractLayer {
     toTime: Date,
     reqConfig?: RequestConfiguration,
   ): Promise<Date[]> {
-    // http://cite.opengeospatial.org/OGCTestData/wms/1.1.1/spec/wms1.1.1.html#dims
-    const capabilities = await fetchGetCapabilitiesXml(this.baseUrl, reqConfig);
-    const layer = capabilities.WMS_Capabilities.Capability[0].Layer[0].Layer.find(
-      layerInfo => this.layerId === layerInfo.Name[0],
-    );
-    if (!layer) {
-      throw new Error('Layer not found');
-    }
-    if (!layer.Dimension) {
-      throw new Error('Layer does not supply time information (no Dimension field)');
-    }
-    const timeDimension = layer.Dimension.find(d => d['$'].name === 'time');
-    if (!timeDimension) {
-      throw new Error("Layer does not supply time information (no Dimension field with name === 'time')");
-    }
-    // http://cite.opengeospatial.org/OGCTestData/wms/1.1.1/spec/wms1.1.1.html#date_time
-    if (timeDimension['$'].units !== 'ISO8601') {
-      throw new Error('Layer time information is not in ISO8601 format, parsing not supported');
-    }
-
-    let allTimesMomentUTC = [];
-    const times = timeDimension['_'].split(',');
-    for (let i = 0; i < times.length; i++) {
-      const timeParts = times[i].split('/');
-      switch (timeParts.length) {
-        case 1:
-          allTimesMomentUTC.push(moment.utc(timeParts[0]));
-          break;
-        case 3:
-          const [intervalFromTime, intervalToTime, intervalDuration] = timeParts;
-          const intervalFromTimeMoment = moment.utc(intervalFromTime);
-          const intervalToTimeMoment = moment.utc(intervalToTime);
-          const intervalDurationMoment = moment.duration(intervalDuration);
-          for (
-            let t = intervalFromTimeMoment;
-            t.isSameOrBefore(intervalToTimeMoment);
-            t.add(intervalDurationMoment)
-          ) {
-            allTimesMomentUTC.push(t.clone());
-          }
-          break;
-        default:
-          throw new Error('Unable to parse time information');
+    const dates = await ensureTimeout(async innerConfig => {
+      // http://cite.opengeospatial.org/OGCTestData/wms/1.1.1/spec/wms1.1.1.html#dims
+      const capabilities = await fetchGetCapabilitiesXml(this.baseUrl, innerConfig);
+      const layer = capabilities.WMS_Capabilities.Capability[0].Layer[0].Layer.find(
+        layerInfo => this.layerId === layerInfo.Name[0],
+      );
+      if (!layer) {
+        throw new Error('Layer not found');
       }
-    }
+      if (!layer.Dimension) {
+        throw new Error('Layer does not supply time information (no Dimension field)');
+      }
+      const timeDimension = layer.Dimension.find(d => d['$'].name === 'time');
+      if (!timeDimension) {
+        throw new Error("Layer does not supply time information (no Dimension field with name === 'time')");
+      }
+      // http://cite.opengeospatial.org/OGCTestData/wms/1.1.1/spec/wms1.1.1.html#date_time
+      if (timeDimension['$'].units !== 'ISO8601') {
+        throw new Error('Layer time information is not in ISO8601 format, parsing not supported');
+      }
 
-    const found: Moment[] = allTimesMomentUTC.filter(t =>
-      t.isBetween(moment.utc(fromTime), moment.utc(toTime), null, '[]'),
-    );
-    found.sort((a, b) => b.unix() - a.unix());
-    return found.map(m => m.toDate());
+      let allTimesMomentUTC = [];
+      const times = timeDimension['_'].split(',');
+      for (let i = 0; i < times.length; i++) {
+        const timeParts = times[i].split('/');
+        switch (timeParts.length) {
+          case 1:
+            allTimesMomentUTC.push(moment.utc(timeParts[0]));
+            break;
+          case 3:
+            const [intervalFromTime, intervalToTime, intervalDuration] = timeParts;
+            const intervalFromTimeMoment = moment.utc(intervalFromTime);
+            const intervalToTimeMoment = moment.utc(intervalToTime);
+            const intervalDurationMoment = moment.duration(intervalDuration);
+            for (
+              let t = intervalFromTimeMoment;
+              t.isSameOrBefore(intervalToTimeMoment);
+              t.add(intervalDurationMoment)
+            ) {
+              allTimesMomentUTC.push(t.clone());
+            }
+            break;
+          default:
+            throw new Error('Unable to parse time information');
+        }
+      }
+
+      const found: Moment[] = allTimesMomentUTC.filter(t =>
+        t.isBetween(moment.utc(fromTime), moment.utc(toTime), null, '[]'),
+      );
+      found.sort((a, b) => b.unix() - a.unix());
+      return found.map(m => m.toDate());
+    }, reqConfig);
+    return dates;
   }
 
   public async updateLayerFromServiceIfNeeded(reqConfig?: RequestConfiguration): Promise<void> {
-    if (this.legendUrl) {
-      return;
-    }
-    if (this.baseUrl === null || this.layerId === null) {
-      throw new Error(
-        "Additional data can't be fetched from service because baseUrl and layerId are not defined",
+    await ensureTimeout(async innerConfig => {
+      if (this.legendUrl) {
+        return;
+      }
+      if (this.baseUrl === null || this.layerId === null) {
+        throw new Error(
+          "Additional data can't be fetched from service because baseUrl and layerId are not defined",
+        );
+      }
+      const capabilities = await fetchGetCapabilitiesXml(this.baseUrl, innerConfig);
+      const layer = capabilities.WMS_Capabilities.Capability[0].Layer[0].Layer.find(
+        layer => this.layerId === layer.Name[0],
       );
-    }
-    const capabilities = await fetchGetCapabilitiesXml(this.baseUrl, reqConfig);
-    const layer = capabilities.WMS_Capabilities.Capability[0].Layer[0].Layer.find(
-      layer => this.layerId === layer.Name[0],
-    );
-    if (!layer) {
-      throw new Error('Layer not found');
-    }
-    const legendUrl =
-      layer.Style && layer.Style[0].LegendURL
-        ? layer.Style[0].LegendURL[0].OnlineResource[0]['$']['xlink:href']
-        : layer.Layer && layer.Layer[0].Style && layer.Layer[0].Style[0].LegendURL
-        ? layer.Layer[0].Style[0].LegendURL[0].OnlineResource[0]['$']['xlink:href']
-        : null;
-    this.legendUrl = legendUrl;
+      if (!layer) {
+        throw new Error('Layer not found');
+      }
+      const legendUrl =
+        layer.Style && layer.Style[0].LegendURL
+          ? layer.Style[0].LegendURL[0].OnlineResource[0]['$']['xlink:href']
+          : layer.Layer && layer.Layer[0].Style && layer.Layer[0].Style[0].LegendURL
+          ? layer.Layer[0].Style[0].LegendURL[0].OnlineResource[0]['$']['xlink:href']
+          : null;
+      this.legendUrl = legendUrl;
+    }, reqConfig);
   }
 }
