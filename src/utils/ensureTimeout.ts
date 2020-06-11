69c385fdfec304b170e827b744c281b13534963e
shooshte <miha.sustersic.work@gmail.com>
2020-06-11 10:03:07 +0200

    Cleanup and comments.

--- src/utils/ensureTimeout.ts
+++ src/utils/ensureTimeout.ts
@@ -1,30 +1,29 @@
 import { CancelToken, RequestConfiguration } from './cancelRequests';
 
+// a wrapper function that ensures network requests triggered by the inner function get cancelled after the specified timeout
+// the wrapper function will receive a single argument - a RequestConfiguration object that should be used to trigger all axios network requests
 export const ensureTimeout = async (
   innerFunction: (requestConfig: RequestConfiguration) => Promise<any>,
   reqConfig?: RequestConfiguration,
 ): Promise<any> => {
   if (!reqConfig || !reqConfig.timeout) {
-    const innerResult = await innerFunction(reqConfig);
-    return innerResult;
+    // if timeout was not specified, call the passed function with the original config
+    return await innerFunction(reqConfig);
   }
 
-  const newReqConfig = { ...reqConfig };
-  const { cancelToken, timeout } = newReqConfig;
-
-  if (!cancelToken) {
-    const token = new CancelToken();
-    newReqConfig.cancelToken = token;
-  }
+  const innerReqConfig: RequestConfiguration = {
+    ...reqConfig,
+    cancelToken: reqConfig.cancelToken ? reqConfig.cancelToken : new CancelToken(),
+    // delete the timeout in case innerFunction has a nested ensureTimeout in order to prevent unnecessary setTimeout calls
+    timeout: undefined,
+  };
 
   const timer = setTimeout(() => {
-    newReqConfig.cancelToken.cancel();
-  }, timeout);
+    innerReqConfig.cancelToken.cancel();
+  }, reqConfig.timeout);
 
   try {
-    // delete the timeout in case innerFunction has a nested ensureTimeout in order to prevent unnecessary setTimeout calls
-    delete newReqConfig.timeout;
-    const resolvedValue = await innerFunction(newReqConfig);
+    const resolvedValue = await innerFunction(innerReqConfig);
     clearTimeout(timer);
     return resolvedValue;
   } catch (e) {
