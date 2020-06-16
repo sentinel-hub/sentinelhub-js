b592335474725b39b41f44169f633fde2d6f0389
Anze Skerlavaj <anze.skerlavaj@sinergise.com>
2020-06-16 09:11:30 +0200

    Cover retries with more integration tests

--- src/layer/__tests__/retries.ts
+++ src/layer/__tests__/retries.ts
@@ -6,30 +6,78 @@ import { constructFixtureFindTiles } from './retries.fixtures';
 
 const mockNetwork = new MockAdapter(axios);
 
-test('S1GRDLayer.findTiles retries correctly on network errors', async () => {
+test('Retries correctly on network errors', async () => {
   // we need to adjust jest timeout until we have support for setting the delay when retrying,
   // otherwise the test will time out:
   jest.setTimeout(7000);
-
-  mockNetwork.onPost().replyOnce(500);
-  mockNetwork.onPost().replyOnce(500);
   const {
-    mockedResponse,
-    expectedResultTiles,
-    expectedResultHasMore,
     fromTime,
     toTime,
     bbox,
     layer,
+    mockedResponse,
+    expectedResultTiles,
+    expectedResultHasMore,
   } = constructFixtureFindTiles({});
+
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
   mockNetwork.onPost().replyOnce(200, mockedResponse);
 
   const { tiles, hasMore } = await layer.findTiles(bbox, fromTime, toTime);
 
-  // check that 3 network calls were made:
   expect(mockNetwork.history.post.length).toBe(3);
-
-  // check final response:
-  expect(hasMore).toBe(expectedResultHasMore);
   expect(tiles).toStrictEqual(expectedResultTiles);
+  expect(hasMore).toBe(expectedResultHasMore);
+});
+
+test("Doesn't retry if not needed", async () => {
+  const {
+    fromTime,
+    toTime,
+    bbox,
+    layer,
+    mockedResponse,
+    expectedResultTiles,
+    expectedResultHasMore,
+  } = constructFixtureFindTiles({});
+
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(200, mockedResponse);
+
+  const { tiles, hasMore } = await layer.findTiles(bbox, fromTime, toTime);
+
+  expect(mockNetwork.history.post.length).toBe(1);
+  expect(tiles).toStrictEqual(expectedResultTiles);
+  expect(hasMore).toBe(expectedResultHasMore);
+});
+
+test('Fails if max. retries (default == 2) was exceeded', async () => {
+  jest.setTimeout(2 * 3000 + 1000);
+  const { fromTime, toTime, bbox, layer } = constructFixtureFindTiles({});
+
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
+
+  await expect(layer.findTiles(bbox, fromTime, toTime)).rejects.toThrow();
+
+  expect(mockNetwork.history.post.length).toBe(3);
+});
+
+test('Fails if max. retries (explicitly set) was exceeded', async () => {
+  jest.setTimeout(3 * 3000 + 1000);
+  const { fromTime, toTime, bbox, layer } = constructFixtureFindTiles({});
+
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().replyOnce(500);
+
+  await expect(layer.findTiles(bbox, fromTime, toTime, null, null, { retries: 3 })).rejects.toThrow();
+
+  expect(mockNetwork.history.post.length).toBe(4);
 });
