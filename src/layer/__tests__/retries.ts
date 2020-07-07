5a5b033be3e778ca3ac338a0271e9113a8ab8fc3
Gustav <gustav.rensburg@sinergise.com>
2020-07-07 14:22:01 +0200

    rename fixtures file to reflect type off call it is making

--- src/layer/__tests__/retries.ts
+++ src/layer/__tests__/retries.ts
@@ -2,7 +2,7 @@ import 'jest-setup';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-import { constructFixtureFindTiles } from './retries.fixtures';
+import { constructFixtureFindTiles } from './fixtures.findTiles';
 
 const mockNetwork = new MockAdapter(axios);
 
