2b70d04ffbde9f55411d0d21b8ecea5995d580f6
Gustav Rensburg <grensburg@sinergise.com>
2020-07-08 15:27:16 +0200

    add initial fixture for getMap

---
+++ src/layer/__tests__/fixtures.getMap.ts
@@ -0,0 +1,31 @@
+import { S2L2ALayer, BBox, CRS_EPSG4326 } from 'src';
+import { MimeTypes } from '../const';
+
+export function constructFixtureGetMap(): Record<any, any> {
+  const layer = new S2L2ALayer({
+    evalscript: '//VERSION=3\nreturn [B02, B02, B02];',
+    maxCloudCoverPercent: 100,
+  });
+  const getMapParams = {
+    bbox: new BBox(CRS_EPSG4326, 18, 20, 20, 22),
+    fromTime: new Date(Date.UTC(2019, 11 - 1, 22, 0, 0, 0)),
+    toTime: new Date(Date.UTC(2019, 12 - 1, 22, 23, 59, 59)),
+    width: 512,
+    height: 512,
+    format: MimeTypes.JPEG,
+  };
+
+  const u8 = new Uint8Array([21, 31, 25, 255, 210, 81, 45, 255]);
+  const blob = new Blob([u8], { type: 'image/jpeg' });
+  const mockedResponse = (config: any): any => {
+    if (config.responseType === 'Blob') {
+      return [200, blob];
+    }
+  };
+
+  return {
+    layer: layer,
+    getMapParams: getMapParams,
+    mockedResponse: mockedResponse,
+  };
+}
