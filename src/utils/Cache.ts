27e755b541a3b3520773862512e3abf716a8a865
Gustav Rensburg <grensburg@sinergise.com>
2020-07-17 10:56:12 +0200

    only invalidate targets if available

--- src/utils/Cache.ts
+++ src/utils/Cache.ts
@@ -111,6 +111,9 @@ class CacheApi implements ShCache {
 export async function invalidateCaches(optionalTargets?: CacheTargets): Promise<void> {
   const targets = optionalTargets || DEFAULT_TARGETS;
   for (const target of targets) {
+    if (!doesTargetExist(target)) {
+      continue;
+    }
     switch (target) {
       case CacheTarget.CACHE_API:
         await new CacheApi().invalidate();
