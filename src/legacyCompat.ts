ae91ec177696729612fa783a3dea446b1f4941c2
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2020-07-28 16:27:13 +0200

    add overrideLayerConstructorParams when calling legacyGetMapFromParams with fallback

--- src/legacyCompat.ts
+++ src/legacyCompat.ts
@@ -111,6 +111,7 @@ export async function legacyGetMapFromParams(
             wmsParams,
             ApiType.WMS,
             fallbackToWmsApi,
+            overrideLayerConstructorParams,
             overrideGetMapParams,
           );
         } else {
