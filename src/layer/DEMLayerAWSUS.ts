db3184c737fda4e58418ab8f63718e9622fab782
dgostencnik <damjan.gostencnik@gmail.com>
2020-12-09 12:45:39 +0100

    fix inconsistent naming

--- src/layer/DEMLayerAWSUS.ts
+++
@@ -1,6 +0,0 @@
-import { DATASET_AWS_DEM } from './dataset';
-import { AbstractDEMLayer } from './AbstractDEMLayer';
-
-export class DEMLayerAWSUS extends AbstractDEMLayer {
-  public readonly dataset = DATASET_AWS_DEM;
-}
