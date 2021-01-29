3aa8af459878bfb71d84a13da03f96f3839c2ab6
Daniel Thiex <daniel.thiex@sinergise.com>
2021-01-29 12:37:24 +0100

    add RTC to storybook

--- stories/s1grdiw.stories.js
+++ stories/s1grdiw.stories.js
@@ -12,6 +12,8 @@ import {
   Polarization,
   Resolution,
   OrbitDirection,
+  BackscatterCoeff,
+  demInstanceType,
   DATASET_AWSEU_S1GRD,
   LayersFactory,
 } from '../dist/sentinelHub.esm';
@@ -203,6 +205,60 @@ export const getMapProcessingWithoutInstance = () => {
       acquisitionMode: AcquisitionMode.IW,
       polarization: Polarization.DV,
       resolution: Resolution.HIGH,
+    });
+
+    const getMapParams = {
+      bbox: bbox3857,
+      fromTime: new Date(Date.UTC(2018, 11 - 1, 22, 0, 0, 0)),
+      toTime: new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59)),
+      width: 512,
+      height: 512,
+      format: MimeTypes.JPEG,
+    };
+    const imageBlob = await layer.getMap(getMapParams, ApiType.PROCESSING);
+    img.src = URL.createObjectURL(imageBlob);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const getMapProcessingWithoutInstanceRTC = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for Processing API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+
+  const img = document.createElement('img');
+  img.width = '512';
+  img.height = '512';
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>GetMap with Processing Radiometric Terrain Correction</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', img);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    const layer = new S1GRDAWSEULayer({
+      evalscript: `
+      //VERSION=3
+      function setup() {
+        return {
+          input: ["VV"],
+          output: { bands: 3 }
+        };
+      }
+
+      function evaluatePixel(sample) {
+        return [2.5 * sample.VV, 2.5 * sample.VV, 2.5 * sample.VV];
+      }
+    `,
+      acquisitionMode: AcquisitionMode.IW,
+      polarization: Polarization.DV,
+      resolution: Resolution.HIGH,
+      backscatterCoeff: BackscatterCoeff.GAMMA0_ELLIPSOID,
+      orthorectify: true,
+      demInstanceType: demInstanceType.MAPZEN,
     });
 
     const getMapParams = {
