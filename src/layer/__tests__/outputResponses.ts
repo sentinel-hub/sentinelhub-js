6235f32f1f68a4015fe4ccb29b68a673ee93e6dd
Ziga Cernigoj <ziga.cernigoj@sinergise.com>
2021-03-02 19:05:38 +0100

    remove fixtues for output responses test

--- src/layer/__tests__/outputResponses.ts
+++ src/layer/__tests__/outputResponses.ts
@@ -1,73 +1,133 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-import { ApiType, setAuthToken, invalidateCaches, S2L2ALayer } from '../../index';
-
+import {
+  ApiType,
+  setAuthToken,
+  invalidateCaches,
+  S2L2ALayer,
+  BBox,
+  CRS_EPSG4326,
+  MimeTypes,
+} from '../../index';
 import '../../../jest-setup';
-import { constructFixtureGetMapOutputResponses } from './fixtures.outputResponses';
 
 const mockNetwork = new MockAdapter(axios);
-
 const EXAMPLE_TOKEN = 'TOKEN111';
+
+const layerWMS = new S2L2ALayer({ instanceId: 'INSTANCE_ID', layerId: 'LAYER_ID' });
+const layerProcessing = new S2L2ALayer({
+  evalscript: `//VERSION=3
+      function setup() {
+        return {
+          input: ["B02", "B03", "B04"],
+          output: [{ id: "default", bands: 4 }, { id: "index", bands: 2 }]
+        };
+      }
+
+      function evaluatePixel(sample) {
+        return {
+            default: [4 * sample.B04, 4 * sample.B03, 4 * sample.B02, sample.dataMask],
+            index: [sample.B04, sample.dataMask] 
+          };
+      }`,
+  maxCloudCoverPercent: 100,
+});
+
+const getMapParams = {
+  bbox: new BBox(CRS_EPSG4326, 18, 20, 20, 22),
+  fromTime: new Date(Date.UTC(2019, 11 - 1, 22, 0, 0, 0)),
+  toTime: new Date(Date.UTC(2019, 12 - 1, 22, 23, 59, 59)),
+  width: 512,
+  height: 512,
+  format: MimeTypes.JPEG,
+};
+const getMapParamsDefaultResponseId = { ...getMapParams, outputResponseId: 'default' };
+const getMapParamsIndexResponseId = { ...getMapParams, format: MimeTypes.PNG, outputResponseId: 'index' };
+const getMapParamsEmptyOutputResponseId = { ...getMapParams, outputResponseId: '' };
+
+const buffer = new ArrayBuffer(8);
+const mockedResponse = (config: any): any => {
+  if (config.responseType === 'arraybuffer') {
+    return [200, buffer];
+  }
+};
 
 beforeEach(async () => {
   await invalidateCaches();
 });
 
-test('Error if outputResponseId is empty string in getMapParams', async () => {
-  const { layer, getMapParamsEmptyOutputResponseId } = constructFixtureGetMapOutputResponses();
-  setAuthToken(EXAMPLE_TOKEN);
-  try {
-    await layer.getMap(getMapParamsEmptyOutputResponseId, ApiType.PROCESSING);
-  } catch (e) {
-    expect(e.message).toBe('outputResponseId most not be empty');
-  }
-});
-
 test('Error if outputResponseId is used with WMS', async () => {
-  const { getMapParamsDefaultResponseId } = constructFixtureGetMapOutputResponses();
   setAuthToken(EXAMPLE_TOKEN);
   try {
-    const layer = new S2L2ALayer({ instanceId: 'INSTANCE_ID', layerId: 'LAYER_ID' });
-    await layer.getMap(getMapParamsDefaultResponseId, ApiType.WMS);
+    await layerWMS.getMap(getMapParamsDefaultResponseId, ApiType.WMS);
   } catch (e) {
     expect(e.message).toBe('outputResponseId is only available with Processing API');
   }
 });
 
 test('Error if outputResponseId is empty and used with WMS', async () => {
-  const { getMapParamsEmptyOutputResponseId } = constructFixtureGetMapOutputResponses();
   setAuthToken(EXAMPLE_TOKEN);
   try {
-    const layer = new S2L2ALayer({ instanceId: 'INSTANCE_ID', layerId: 'LAYER_ID' });
-    await layer.getMap(getMapParamsEmptyOutputResponseId, ApiType.WMS);
+    await layerWMS.getMap(getMapParamsEmptyOutputResponseId, ApiType.WMS);
   } catch (e) {
     expect(e.message).toBe('outputResponseId is only available with Processing API');
   }
 });
 
-it('Success if outputResponseId is set correctly in getMapParams', async () => {
+it('NO output response id, JPEG format', async () => {
   // arrayBuffer needs to be used, and removing this will cause getMap to fetch a blob, as window.Blob was created with jsdom
   window.Blob = undefined;
-  const { layer, getMapParamsDefaultResponseId, mockedResponse } = constructFixtureGetMapOutputResponses();
+
   setAuthToken(EXAMPLE_TOKEN);
   mockNetwork.reset();
   mockNetwork.onPost().replyOnce(200, mockedResponse);
 
-  await layer.getMap(getMapParamsDefaultResponseId, ApiType.PROCESSING);
+  await layerProcessing.getMap(getMapParams, ApiType.PROCESSING);
 
-  expect(mockNetwork.history.post.length).toBe(1);
+  const request = mockNetwork.history.post[0];
+  const requestData = JSON.parse(request.data);
+  expect(requestData.output.responses).toEqual([{ identifier: 'default', format: { type: 'image/jpeg' } }]);
 });
 
-it('Success if outputResponseId is not set in getMapParams', async () => {
+it('EMPTY output response id, JPEG format', async () => {
   // arrayBuffer needs to be used, and removing this will cause getMap to fetch a blob, as window.Blob was created with jsdom
   window.Blob = undefined;
-  const { layer, getMapParams, mockedResponse } = constructFixtureGetMapOutputResponses();
   setAuthToken(EXAMPLE_TOKEN);
   mockNetwork.reset();
   mockNetwork.onPost().replyOnce(200, mockedResponse);
 
-  await layer.getMap(getMapParams, ApiType.PROCESSING);
+  await layerProcessing.getMap(getMapParamsEmptyOutputResponseId, ApiType.PROCESSING);
 
-  expect(mockNetwork.history.post.length).toBe(1);
+  const request = mockNetwork.history.post[0];
+  const requestData = JSON.parse(request.data);
+  expect(requestData.output.responses).toEqual([{ identifier: 'default', format: { type: 'image/jpeg' } }]);
+});
+
+it('DEFAULT output response, JPEG format', async () => {
+  // arrayBuffer needs to be used, and removing this will cause getMap to fetch a blob, as window.Blob was created with jsdom
+  window.Blob = undefined;
+  setAuthToken(EXAMPLE_TOKEN);
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(200, mockedResponse);
+
+  await layerProcessing.getMap(getMapParamsDefaultResponseId, ApiType.PROCESSING);
+
+  const request = mockNetwork.history.post[0];
+  const requestData = JSON.parse(request.data);
+  expect(requestData.output.responses).toEqual([{ identifier: 'default', format: { type: 'image/jpeg' } }]);
+});
+
+it('INDEX output response, PNG format', async () => {
+  // arrayBuffer needs to be used, and removing this will cause getMap to fetch a blob, as window.Blob was created with jsdom
+  window.Blob = undefined;
+  setAuthToken(EXAMPLE_TOKEN);
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(200, mockedResponse);
+
+  await layerProcessing.getMap(getMapParamsIndexResponseId, ApiType.PROCESSING);
+
+  const request = mockNetwork.history.post[0];
+  const requestData = JSON.parse(request.data);
+  expect(requestData.output.responses).toEqual([{ identifier: 'index', format: { type: 'image/png' } }]);
 });
