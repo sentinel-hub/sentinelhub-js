fa6f10b467a76fac4830855c29beba0067ef6ddc
dgostencnik <damjan.gostencnik@gmail.com>
2021-04-19 08:02:56 +0200

    add previews for LOTL1

--- src/layer/Landsat8AWSLOTL1Layer.ts
+++ src/layer/Landsat8AWSLOTL1Layer.ts
@@ -5,11 +5,19 @@ import { Link, LinkType } from './const';
 export class Landsat8AWSLOTL1Layer extends AbstractLandsat8Layer {
   public readonly dataset = DATASET_AWS_LOTL1;
 
+  private getPreviewUrl(productId: string): string {
+    return `https://landsatlook.usgs.gov/gen-browse?size=thumb&type=refl&product_id=${productId}`;
+  }
+
   protected getTileLinks(tile: Record<string, any>): Link[] {
     return [
       {
         target: tile.dataUri,
         type: LinkType.AWS,
+      },
+      {
+        target: this.getPreviewUrl(tile.originalId),
+        type: LinkType.PREVIEW,
       },
     ];
   }
@@ -21,6 +29,11 @@ export class Landsat8AWSLOTL1Layer extends AbstractLandsat8Layer {
     if (assets && assets.data) {
       result.push({ target: assets.data.href, type: LinkType.AWS });
     }
+
+    result.push({
+      target: this.getPreviewUrl(feature.id),
+      type: LinkType.PREVIEW,
+    });
 
     return result;
   }
