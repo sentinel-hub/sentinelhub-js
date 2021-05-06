ab0ffb0703ac81e582712db4d2be5de3b9bba0a8
dgostencnik <damjan.gostencnik@gmail.com>
2021-05-06 13:55:07 +0200

    add test for retrying concurrent requests

--- src/layer/__tests__/retries.ts
+++ src/layer/__tests__/retries.ts
@@ -145,3 +145,44 @@ test('Retries correctly when caching is enabled', async () => {
   expect(mockNetwork.history.post.length).toBe(3);
   expect(cachedResult.tiles).toStrictEqual(expectedResultTiles);
 });
+
+test('Retries concurrent requests when caching is enabled', async () => {
+  jest.setTimeout(10 * 3000 + 1000);
+  const {
+    fromTime,
+    toTime,
+    bbox,
+    layer,
+    mockedResponse,
+    expectedResultTiles,
+    expectedResultHasMore,
+  } = constructFixtureFindTiles({});
+
+  //first request will fail, all other will succeed
+  mockNetwork.reset();
+  mockNetwork.onPost().replyOnce(500);
+  mockNetwork.onPost().reply(200, mockedResponse);
+
+  //create 3 identical requests
+  const findTilesRequest = layer.findTiles(bbox, fromTime, toTime, null, null, {
+    retries: 3,
+    cache: CACHE_CONFIG_30MIN,
+  });
+  const requests = [findTilesRequest, findTilesRequest, findTilesRequest];
+
+  const results: any[] = [];
+  await Promise.all(
+    requests.map(async request => {
+      const result = await request;
+      results.push(result);
+    }),
+  );
+
+  // since caching is enabled, there should be only 2 "network" requests - one successful and one 500
+  expect(mockNetwork.history.post.length).toBe(2);
+  expect(results.length).toBe(3);
+  results.forEach(result => {
+    expect(result.tiles).toStrictEqual(expectedResultTiles);
+    expect(result.hasMore).toBe(expectedResultHasMore);
+  });
+});
