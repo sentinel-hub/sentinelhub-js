9f67adfdd68f47b8fa1933ba04543a4b2161f5c4
dgostencnik <damjan.gostencnik@gmail.com>
2021-05-31 23:59:47 +0200

    add search

--- src/dataimport/TPDI.ts
+++ src/dataimport/TPDI.ts
@@ -1,6 +1,20 @@
 import { RequestConfiguration } from '../utils/cancelRequests';
 import { getQuotasInner } from './quotas';
-import { Quota, TPDICollections } from './const';
+import { Quota, TPDICollections, TPDProvider, TPDISearchParams } from './const';
+import { AirbusDataProvider } from './AirbusDataProvider';
+import { PlanetDataProvider } from './PlanetDataProvider';
+import { MaxarDataProvider } from './MaxarDataProvider';
+import { TPDProviderInterface } from './TPDProvider';
+
+const dataProviders = [new AirbusDataProvider(), new PlanetDataProvider(), new MaxarDataProvider()];
+
+export function getThirdPartyDataProvider(provider: TPDProvider): TPDProviderInterface {
+  const tpdp = dataProviders.find(p => p.getProvider() === provider);
+  if (!tpdp) {
+    throw new Error(`Unknown data provider ${provider}`);
+  }
+  return tpdp;
+}
 
 export class TPDI {
   public static async getQuota(
@@ -15,5 +29,14 @@ export class TPDI {
 
   public static async getQuotas(reqConfig?: RequestConfiguration): Promise<Quota> {
     return await getQuotasInner(null, reqConfig);
+  }
+
+  public static async search(
+    provider: TPDProvider,
+    params: TPDISearchParams,
+    reqConfig?: RequestConfiguration,
+  ): Promise<any> {
+    const tpdp = getThirdPartyDataProvider(provider);
+    return await tpdp.search(params, reqConfig);
   }
 }
