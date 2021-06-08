ae1a369970a5cee9aaac414c7fde9d428353de9d
dgostencnik <damjan.gostencnik@gmail.com>
2021-06-08 19:42:48 +0200

    add stories for quotas, search, thumbnails

---
+++ stories/TPDI.stories.js
@@ -0,0 +1,183 @@
+import { setAuthTokenWithOAuthCredentials } from './storiesUtils';
+
+import {
+  AirbusConstellation,
+  CRS_EPSG4326,
+  BBox,
+  TPDProvider,
+  TPDICollections,
+  TPDI,
+} from '../dist/sentinelHub.esm';
+
+export default {
+  title: 'TPDI search',
+};
+
+const renderSearchResults = (containerEl, features) =>
+  features.forEach(product => {
+    const ul = document.createElement('ul');
+    containerEl.appendChild(ul);
+    for (let key in product) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      let text;
+      if (product[key] instanceof Object) {
+        text = JSON.stringify(product[key]);
+      } else {
+        text = product[key];
+      }
+      li.innerHTML = `${key} : ${text}`;
+    }
+  });
+
+const defaultSearchParams = {
+  fromTime: new Date(2017, 1 - 1, 1, 0, 0, 0, 0),
+  toTime: new Date(2018, 6 - 1, 31, 23, 59, 59, 999),
+  bbox: new BBox(CRS_EPSG4326, -97.370012, 32.733646, -97.361003, 32.744187),
+};
+
+export const GetQuotas = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for TPDI API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+  const containerEl = document.createElement('pre');
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>Get TPDI quotas</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', containerEl);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    const quotas = await TPDI.getQuotas();
+
+    quotas.forEach(quota => {
+      const ul = document.createElement('ul');
+      containerEl.appendChild(ul);
+      for (let key in quota) {
+        const li = document.createElement('li');
+        ul.appendChild(li);
+        let text;
+        if (quota[key] instanceof Object) {
+          text = JSON.stringify(quota[key]);
+        } else {
+          text = quota[key];
+        }
+        li.innerHTML = `${key} : ${text}`;
+      }
+    });
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const SearchAirbusSpot = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for TPDI API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+  const containerEl = document.createElement('pre');
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>Search Airbus SPOT</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', containerEl);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    defaultSearchParams.constellation = AirbusConstellation.SPOT;
+    const { features, links } = await TPDI.search(TPDProvider.AIRBUS, defaultSearchParams, {}, 5);
+    renderSearchResults(containerEl, features);
+    const nextPage = document.createElement('div');
+    nextPage.innerHTML = '<p>next page</p>';
+    containerEl.appendChild(nextPage);
+    const response = await TPDI.search(TPDProvider.AIRBUS, defaultSearchParams, {}, 5, links.nextToken);
+    renderSearchResults(containerEl, response.features);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const SearchAirbusPleiades = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for TPDI API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+  const containerEl = document.createElement('pre');
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>Search Airbus Pleiades</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', containerEl);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+    defaultSearchParams.constellation = AirbusConstellation.PHR;
+    const { features, links } = await TPDI.search(TPDProvider.AIRBUS, defaultSearchParams, {}, 5);
+    renderSearchResults(containerEl, features);
+    const nextPage = document.createElement('div');
+    nextPage.innerHTML = '<p>next page</p>';
+    containerEl.appendChild(nextPage);
+    const response = await TPDI.search(TPDProvider.AIRBUS, defaultSearchParams, {}, 5, links.nextToken);
+    renderSearchResults(containerEl, response.features);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const SearchMAXAR = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for TPDI API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+  const containerEl = document.createElement('pre');
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>Search MAXAR</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', containerEl);
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+    defaultSearchParams.constellation = AirbusConstellation.PHR;
+    const { features } = await TPDI.search(TPDProvider.MAXAR, defaultSearchParams, {}, 5);
+    renderSearchResults(containerEl, features);
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
+
+export const GetThumbnails = () => {
+  if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    return "<div>Please set OAuth Client's id and secret for TPDI API (CLIENT_ID, CLIENT_SECRET env vars)</div>";
+  }
+  const containerEl = document.createElement('pre');
+
+  const wrapperEl = document.createElement('div');
+  wrapperEl.innerHTML = '<h2>get thumbnails</h2>';
+  wrapperEl.insertAdjacentElement('beforeend', containerEl);
+  const imgs = [];
+  const products = [
+    { collectionId: TPDICollections.AIRBUS_SPOT, productId: '1e2d3f96-5afc-4050-8e74-98e1eb159cd1' },
+    { collectionId: TPDICollections.AIRBUS_PLEIADES, productId: 'ec30b314-e423-4780-a3a9-44b03c964d96' },
+    { collectionId: TPDICollections.MAXAR_WORLDVIEW, productId: '10300100638BEC00' },
+  ];
+  for (let i = 0; i < products.length; i++) {
+    const img = document.createElement('img');
+    img.width = '256';
+    img.height = '256';
+    imgs.push(img);
+    containerEl.insertAdjacentElement('beforeend', img);
+  }
+
+  const perform = async () => {
+    await setAuthTokenWithOAuthCredentials();
+
+    for (let i = 0; i < products.length; i++) {
+      const imageBlob = await TPDI.getThumbnail(products[i].collectionId, products[i].productId);
+      imgs[i].src = URL.createObjectURL(imageBlob);
+    }
+  };
+  perform().then(() => {});
+
+  return wrapperEl;
+};
