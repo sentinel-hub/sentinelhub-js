5f74e0033a5ad61b48b41df97db41d6bb3106fb4
dgostencnik <damjan.gostencnik@gmail.com>
2021-08-09 17:34:09 +0200

    extend correct layer

--- src/layer/Landsat7AWSLETML1Layer.ts
+++ src/layer/Landsat7AWSLETML1Layer.ts
@@ -1,6 +1,6 @@
 import { DATASET_AWS_LETML1 } from './dataset';
-import { AbstractLandsat8Layer } from './AbstractLandsat8Layer';
+import { AbstractSentinelHubV3WithCCLayer } from './AbstractSentinelHubV3WithCCLayer';
 
-export class Landsat7AWSLETML1Layer extends AbstractLandsat8Layer {
+export class Landsat7AWSLETML1Layer extends AbstractSentinelHubV3WithCCLayer {
   public readonly dataset = DATASET_AWS_LETML1;
 }
