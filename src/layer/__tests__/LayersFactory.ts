2fbb02cacfae721552c7a970baabfa5f8a6149c0
grensburg <gustav.rensburg@sinergise.com>
2021-11-15 13:26:25 +0100

    add LayersFactory tests

---
+++ src/layer/__tests__/LayersFactory.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { DATASET_S2L2A, DATASET_S5PL2 } from '../dataset';
+import { LayersFactory } from '../LayersFactory';
+import { WmsLayer, setAuthToken } from '../../index';
+import { S2L1CLayer } from '../S2L1CLayer';
+import { S5PL2Layer } from '../S5PL2Layer';
+import { getCapabilitiesXmlResponse } from './fixtures.wmsGetCapabilities';
+
+const mockNetwork = new MockAdapter(axios);
+type CASE = [string, any, any];
+
+const cases: CASE[] = [
+  [
+    `${DATASET_S2L2A.shServiceHostname}ogc/wms/instanceID`,
+    {
+      layers: [
+        {
+          id: 'S2L1C',
+          name: 's2l1c',
+          description: '',
+          dataset: 'S2L1C',
+          legendUrl: '',
+        },
+      ],
+    },
+    S2L1CLayer,
+  ],
+  [
+    `${DATASET_S5PL2.shServiceHostname}ogc/wms/`,
+    {
+      layers: [
+        {
+          dataset: 'S5PL2',
+          description: '',
+          id: 'S5TMP',
+          legendUrl: null,
+          name: 's5tmp',
+        },
+      ],
+    },
+    S5PL2Layer,
+  ],
+  [
+    'https://proba-v-mep.esa.int/applications/geo-viewer/app/geoserver/ows',
+    getCapabilitiesXmlResponse,
+    WmsLayer,
+  ],
+];
+
+describe('Test LayersFactory', () => {
+  test.each(cases)('Given url returns correct Layer type', async (url, response, expectedResult) => {
+    setAuthToken('a123');
+    mockNetwork.reset();
+    mockNetwork.onAny().replyOnce(200, response); // we don't care about the response, we will just inspect the request params
+    const layer = (await LayersFactory.makeLayers(url, null))[0];
+
+    expect(layer).toBeInstanceOf(expectedResult);
+  });
+});
