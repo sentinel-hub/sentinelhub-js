464b33ba9933b58ba584725e327c2cc264ab8963
guss84 <grensburg@gmail.com>
2021-12-03 12:31:51 +0100

    rename vars

--- src/layer/wmts.utils.ts
+++ src/layer/wmts.utils.ts
@@ -4,9 +4,9 @@ import { CRS_EPSG3857, CRS_EPSG4326 } from '../crs';
 import { OgcServiceTypes } from './const';
 import { fetchGetCapabilitiesXml, GetCapabilitiesXmlLayer } from './utils';
 
-const D2R = Math.PI / 180;
-const R2D = 180 / Math.PI;
-const A = 6378137.0;
+const DEGREE_TO_RADIAN = Math.PI / 180;
+const RADIAN_TO_DEGREE = 180 / Math.PI;
+const EQUATOR_RADIUS = 6378137.0;
 
 export type GetCapabilitiesWmtsXml = {
   Capabilities: {
@@ -30,7 +30,10 @@ export type GetCapabilitiesXmlWmtsLayer = {
 };
 
 function toWgs84(xy: number[]): number[] {
-  return [(xy[0] * R2D) / A, (Math.PI * 0.5 - 2.0 * Math.atan(Math.exp(-xy[1] / A))) * R2D];
+  return [
+    (xy[0] * RADIAN_TO_DEGREE) / EQUATOR_RADIUS,
+    (Math.PI * 0.5 - 2.0 * Math.atan(Math.exp(-xy[1] / EQUATOR_RADIUS))) * RADIAN_TO_DEGREE,
+  ];
 }
 
 export function bboxToXyz(bbox: BBox, zoom: number, tileSize: number): { x: number; y: number; z: number } {
@@ -39,17 +42,17 @@ export function bboxToXyz(bbox: BBox, zoom: number, tileSize: number): { x: numb
     const bottomRight = toWgs84([bbox.maxX, bbox.maxY]);
     bbox = new BBox(CRS_EPSG4326, topLeft[0], topLeft[1], bottomRight[0], bottomRight[1]);
   }
-  const topLeft = [bbox.minX, bbox.maxY]; // lower left
+  const topLeft = [bbox.minX, bbox.maxY];
   const { pixelX, pixelY } = toPixel(topLeft, tileSize, zoom);
   const tileX = Math.floor(pixelX / tileSize);
   const tileY = Math.floor(pixelY / tileSize);
   return { x: tileX, y: tileY, z: zoom };
 }
 
-export function toPixel(ll: number[], tileSize: number, zoom: number): { pixelX: number; pixelY: number } {
-  const [longitude, latitude] = ll;
+export function toPixel(coord: number[], tileSize: number, zoom: number): { pixelX: number; pixelY: number } {
+  const [longitude, latitude] = coord;
   const mapWidth = tileSize * Math.pow(2, zoom);
-  const sinLatitude = Math.min(Math.max(Math.sin(D2R * latitude), -0.9999), 0.9999);
+  const sinLatitude = Math.min(Math.max(Math.sin(DEGREE_TO_RADIAN * latitude), -0.9999), 0.9999);
   const pixelX = ((longitude + 180) / 360) * mapWidth;
   const pixelY = (0.5 - Math.log((1 + sinLatitude) / (1 - sinLatitude)) / (4 * Math.PI)) * mapWidth;
   return { pixelX, pixelY };
