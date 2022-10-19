968689c801cefba4cc8f0158841198487a44a39a
Mitja Kuscer <mitja.kuscer@sinergise.com>
2022-10-19 15:03:47 +0200

    Remove unused overwrites (or so it seems)

--- src/layer/HLSAWSLayer.ts
+++ src/layer/HLSAWSLayer.ts
@@ -1,7 +1,5 @@
 import { DATASET_AWS_HLS } from './dataset';
 import { DataProductId, FindTilesAdditionalParameters, MosaickingOrder } from './const';
-import { RequestConfiguration } from '../utils/cancelRequests';
-import { ProcessingPayload } from './processing';
 import { AbstractSentinelHubV3WithCCLayer } from './AbstractSentinelHubV3WithCCLayer';
 
 interface ConstructorParameters {
@@ -35,73 +33,6 @@ export class HLSAWSLayer extends AbstractSentinelHubV3WithCCLayer {
   public constructor({ constellation = null, ...params }: ConstructorParameters) {
     super(params);
     this.constellation = constellation;
-  }
-
-  protected getWmsGetMapUrlAdditionalParameters(): Record<string, any> {
-    return {
-      ...super.getWmsGetMapUrlAdditionalParameters(),
-      constellation: this.constellation,
-    };
-  }
-
-  public async _updateProcessingGetMapPayload(
-    payload: ProcessingPayload,
-    datasetSeqNo: number = 0,
-  ): Promise<ProcessingPayload> {
-    payload = await super._updateProcessingGetMapPayload(payload);
-    payload.input.data[datasetSeqNo].dataFilter.maxCloudCoverage = this.maxCloudCoverPercent;
-    payload.input.data[datasetSeqNo].dataFilter.constellation = this.constellation;
-    return payload;
-  }
-
-  protected extractFindTilesMetaFromCatalog(feature: Record<string, any>): Record<string, any> {
-    let result: Record<string, any> = {};
-
-    if (!feature) {
-      return result;
-    }
-
-    result = {
-      ...super.extractFindTilesMetaFromCatalog(feature),
-      cloudCoverPercent: feature.properties['eo:cloud_cover'],
-    };
-
-    return result;
-  }
-
-  protected async getFindDatesUTCAdditionalParameters(
-    reqConfig: RequestConfiguration, // eslint-disable-line @typescript-eslint/no-unused-vars
-  ): Promise<Record<string, any>> {
-    const result: Record<string, any> = {
-      datasetParameters: {
-        type: this.dataset.datasetParametersType,
-      },
-    };
-
-    if (this.maxCloudCoverPercent !== null && this.maxCloudCoverPercent !== undefined) {
-      result.maxCloudCoverage = this.maxCloudCoverPercent / 100;
-    }
-
-    if (this.constellation !== null && this.constellation !== undefined) {
-      result.datasetParameters.constellation = this.constellation;
-    }
-
-    return result;
-  }
-
-  public getStatsAdditionalParameters(): Record<string, any> {
-    return {
-      maxcc: this.maxCloudCoverPercent,
-      constellation: this.constellation,
-    };
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  protected extractFindTilesMeta(tile: any): Record<string, any> {
-    return {
-      ...super.extractFindTilesMeta(tile),
-      cloudCoverPercent: tile.cloudCoverPercentage,
-    };
   }
 
   protected createCatalogFilterQuery(
