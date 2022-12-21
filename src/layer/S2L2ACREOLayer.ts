74c15f3e1adafa9ab060e4b37d81cff31d4c497f
Mitja Kuscer <mitja.kuscer@sinergise.com>
2022-12-21 15:58:40 +0100

    Update to correct dataset name

--- src/layer/S2L2ACREOLayer.ts
+++ src/layer/S2L2ACREOLayer.ts
@@ -1,6 +1,6 @@
-import { DATASET_CREODIAS_S2L1C } from './dataset';
+import { DATASET_CREODIAS_S2L2A } from './dataset';
 import { S2L2ALayer } from './S2L2ALayer';
 
 export class S2L2ACREOLayer extends S2L2ALayer {
-  public readonly dataset = DATASET_CREODIAS_S2L1C;
+  public readonly dataset = DATASET_CREODIAS_S2L2A;
 }
