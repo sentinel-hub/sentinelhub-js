b26f00d89ef8c2d3aa2a8def5087f4ad5896053d
Mitja Kuscer <mitja.kuscer@sinergise.com>
2023-01-06 14:40:36 +0100

    Add getStats warning to S2 as well

--- src/layer/S2L2ACDASLayer.ts
+++ src/layer/S2L2ACDASLayer.ts
@@ -1,6 +1,12 @@
+import { RequestConfiguration } from '../utils/cancelRequests';
 import { DATASET_CDAS_S2L2A } from './dataset';
 import { S2L2ALayer } from './S2L2ALayer';
 
 export class S2L2ACDASLayer extends S2L2ALayer {
   public readonly dataset = DATASET_CDAS_S2L2A;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async getStats(payload: any, reqConfig?: RequestConfiguration): Promise<any> {
+    throw new Error('getStats() not implemented for this dataset');
+  }
 }
