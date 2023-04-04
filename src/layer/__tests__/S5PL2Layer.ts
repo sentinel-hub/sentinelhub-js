16f845f0ae1c628bc12e5ed09d9b84e616d59596
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2023-04-04 12:27:16 +0200

    adapt S5P tests

--- src/layer/__tests__/S5PL2Layer.ts
+++ src/layer/__tests__/S5PL2Layer.ts
@@ -54,16 +54,20 @@ describe('Test findTiles using searchIndex', () => {
   });
 
   test('searchIndex is used if token is not set', async () => {
-    await checkIfCorrectEndpointIsUsed(null, constructFixtureFindTilesSearchIndex({}), SEARCH_INDEX_URL);
+    await checkIfCorrectEndpointIsUsed(
+      null,
+      constructFixtureFindTilesSearchIndex(S5PL2Layer, {}),
+      SEARCH_INDEX_URL,
+    );
   });
 
   test.each(layerParamsArr)('check if correct request is constructed', async layerParams => {
-    const fixtures = constructFixtureFindTilesSearchIndex(layerParams);
+    const fixtures = constructFixtureFindTilesSearchIndex(S5PL2Layer, layerParams);
     await checkRequestFindTiles(fixtures);
   });
 
   test('response from searchIndex', async () => {
-    await checkResponseFindTiles(constructFixtureFindTilesSearchIndex({}));
+    await checkResponseFindTiles(constructFixtureFindTilesSearchIndex(S5PL2Layer, {}));
   });
 });
 
@@ -74,16 +78,20 @@ describe('Test findTiles using catalog', () => {
   });
 
   test('Catalog is used if token is set', async () => {
-    await checkIfCorrectEndpointIsUsed(AUTH_TOKEN, constructFixtureFindTilesCatalog({}), CATALOG_URL);
+    await checkIfCorrectEndpointIsUsed(
+      AUTH_TOKEN,
+      constructFixtureFindTilesCatalog(S5PL2Layer, {}),
+      CATALOG_URL,
+    );
   });
 
   test.each(layerParamsArr)('check if correct request is constructed', async layerParams => {
-    const fixtures = constructFixtureFindTilesCatalog(layerParams);
+    const fixtures = constructFixtureFindTilesCatalog(S5PL2Layer, layerParams);
     await checkRequestFindTiles(fixtures);
   });
 
   test('response from catalog', async () => {
-    await checkResponseFindTiles(constructFixtureFindTilesCatalog({}));
+    await checkResponseFindTiles(constructFixtureFindTilesCatalog(S5PL2Layer, {}));
   });
 });
 
