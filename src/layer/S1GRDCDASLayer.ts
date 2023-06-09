441869577358561be30d1d802227b3b06701880e
Mitja Kuscer <mitja.kuscer@sinergise.com>
2023-06-09 09:21:37 +0200

    Enable stats for CDAS layers

--- src/layer/S1GRDCDASLayer.ts
+++ src/layer/S1GRDCDASLayer.ts
@@ -1,12 +1,6 @@
-import { RequestConfiguration } from '../utils/cancelRequests';
 import { DATASET_CDAS_S1GRD } from './dataset';
 import { S1GRDAWSEULayer } from './S1GRDAWSEULayer';
 
 export class S1GRDCDASLayer extends S1GRDAWSEULayer {
   public readonly dataset = DATASET_CDAS_S1GRD;
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async getStats(payload: any, reqConfig?: RequestConfiguration): Promise<any> {
-    throw new Error('getStats() not implemented for this dataset');
-  }
 }
--- src/layer/S3OLCICDASLayer.ts
+++ src/layer/S3OLCICDASLayer.ts
@@ -3,8 +3,4 @@ import { S3OLCILayer } from './S3OLCILayer';
 
 export class S3OLCICDASLayer extends S3OLCILayer {
   public readonly dataset = DATASET_CDAS_S3OLCI;
-
-  public async getStats(): Promise<any> {
-    throw new Error('getStats() not implemented for this dataset');
-  }
 }
