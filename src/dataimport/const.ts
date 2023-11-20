5bedf0e8d86ea1b0b1908e83e2efda2103602ddf
Mitja Kuscer <mitja.kuscer@sinergise.com>
2023-11-20 12:17:19 +0100

    Add Planetary variables

--- src/dataimport/const.ts
+++ src/dataimport/const.ts
@@ -8,6 +8,7 @@ export enum TPDICollections {
   PLANET_SCOPE = 'PLANET_SCOPE',
   MAXAR_WORLDVIEW = 'MAXAR_WORLDVIEW',
   PLANET_SKYSAT = 'PLANET_SKYSAT',
+  PLANET_PLANETARY_VARIABLES = 'PLANET_PLANETARY_VARIABLES',
 }
 
 export enum TPDProvider {
@@ -67,6 +68,68 @@ export const PlanetSupportedProductBundles = {
     PlanetProductBundle.ANALYTIC_UDM2,
     PlanetProductBundle.ANALYTIC_SR_UDM2,
     PlanetProductBundle.PANCHROMATIC,
+  ],
+};
+
+export enum PlanetPVType {
+  BiomassProxy = 'biomass_proxy',
+  LandSurfaceTemperature = 'land_surface_temperature',
+  SoilWaterContent = 'soil_water_content',
+  VegetationOpticalDepth = 'vegetation_optical_depth',
+  ForestCarbonDiligence30m = 'forest_carbon_diligence_30m',
+}
+
+export enum PlanetPVId {
+  BIOMASS_PROXY_V3_0_10 = 'BIOMASS-PROXY_V3.0_10',
+  LST_AMSR2_V1_0_100 = 'LST-AMSR2_V1.0_100',
+  LST_AMSRE_V1_0_1000 = 'LST-AMSRE_V1.0_1000',
+  LST_AMSR2_V1_0_1000 = 'LST-AMSR2_V1.0_1000',
+  SWC_AMSR2_C_V1_0_100 = 'SWC-AMSR2-C_V1.0_100',
+  SWC_AMSR2_X_V1_0_100 = 'SWC-AMSR2-X_V1.0_100',
+  SWC_SMAP_L_V1_0_100 = 'SWC-SMAP-L_V1.0_100',
+  SWC_AMSRE_C_V4_0_1000 = 'SWC-AMSRE-C_V4.0_1000',
+  SWC_AMSRE_X_V4_0_1000 = 'SWC-AMSRE-X_V4.0_1000',
+  SWC_AMSR2_C_V4_0_1000 = 'SWC-AMSR2-C_V4.0_1000',
+  SWC_AMSR2_X_V4_0_1000 = 'SWC-AMSR2-X_V4.0_1000',
+  SWC_SMAP_L_V4_0_1000 = 'SWC-SMAP-L_V4.0_1000',
+  VOD_AMSRE_C_V4_0_1000 = 'VOD-AMSRE-C_V4.0_1000',
+  VOD_AMSRE_X_V4_0_1000 = 'VOD-AMSRE-X_V4.0_1000',
+  VOD_AMSR2_C_V4_0_1000 = 'VOD-AMSR2-C_V4.0_1000',
+  VOD_AMSR2_X_V4_0_1000 = 'VOD-AMSR2-X_V4.0_1000',
+  CANOPY_HEIGHT_V1_0_0_30 = 'CANOPY_HEIGHT_v1.0.0_30',
+  CANOPY_COVER_V1_0_0_30 = 'CANOPY_COVER_v1.0.0_30',
+  ABOVEGROUND_CARBON_DENSITY_V1_0_0_30 = 'ABOVEGROUND_CARBON_DENSITY_v1.0.0_30',
+  DAY_OF_YEAR_V1_0_0_30 = 'DAY_OF_YEAR_v1.0.0_30',
+}
+
+export const PlanetSupportedPVIds = {
+  [PlanetPVType.BiomassProxy]: [PlanetPVId.BIOMASS_PROXY_V3_0_10],
+  [PlanetPVType.LandSurfaceTemperature]: [
+    PlanetPVId.LST_AMSR2_V1_0_100,
+    PlanetPVId.LST_AMSRE_V1_0_1000,
+    PlanetPVId.LST_AMSR2_V1_0_1000,
+  ],
+  [PlanetPVType.SoilWaterContent]: [
+    PlanetPVId.SWC_AMSR2_C_V1_0_100,
+    PlanetPVId.SWC_AMSR2_X_V1_0_100,
+    PlanetPVId.SWC_SMAP_L_V1_0_100,
+    PlanetPVId.SWC_AMSRE_C_V4_0_1000,
+    PlanetPVId.SWC_AMSRE_X_V4_0_1000,
+    PlanetPVId.SWC_AMSR2_C_V4_0_1000,
+    PlanetPVId.SWC_AMSR2_X_V4_0_1000,
+    PlanetPVId.SWC_SMAP_L_V4_0_1000,
+  ],
+  [PlanetPVType.VegetationOpticalDepth]: [
+    PlanetPVId.VOD_AMSRE_C_V4_0_1000,
+    PlanetPVId.VOD_AMSRE_X_V4_0_1000,
+    PlanetPVId.VOD_AMSR2_C_V4_0_1000,
+    PlanetPVId.VOD_AMSR2_X_V4_0_1000,
+  ],
+  [PlanetPVType.ForestCarbonDiligence30m]: [
+    PlanetPVId.CANOPY_HEIGHT_V1_0_0_30,
+    PlanetPVId.CANOPY_COVER_V1_0_0_30,
+    PlanetPVId.ABOVEGROUND_CARBON_DENSITY_V1_0_0_30,
+    PlanetPVId.DAY_OF_YEAR_V1_0_0_30,
   ],
 };
 
