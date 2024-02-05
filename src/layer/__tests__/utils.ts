04beed93eb07306880442c169040ec4910493243
Mitja Kuscer <mitja.kuscer@sinergise.com>
2024-02-05 15:25:47 +0100

    Add unit tests

--- src/layer/__tests__/utils.ts
+++ src/layer/__tests__/utils.ts
@@ -1,5 +1,12 @@
+import { BBox } from '../../bbox';
+import { CRS_EPSG3857, CRS_EPSG4326 } from '../../crs';
 import { OgcServiceTypes, SH_SERVICE_ROOT_URL } from '../const';
-import { createGetCapabilitiesXmlUrl, getSHServiceRootUrlFromBaseUrl } from '../utils';
+import {
+  createGetCapabilitiesXmlUrl,
+  ensureMercatorBBox,
+  getSHServiceRootUrlFromBaseUrl,
+  metersPerPixel,
+} from '../utils';
 
 const cases = [
   {
@@ -40,5 +47,95 @@ describe('getSHServiceRootUrlFromBaseUrl', () => {
     ['not url', SH_SERVICE_ROOT_URL.default],
   ])('getSHServiceRootUrlFromBaseUrl %p', async (baseUrl, expected) => {
     expect(getSHServiceRootUrlFromBaseUrl(baseUrl)).toBe(expected);
+  });
+});
+
+describe('ensureMercatorBBox', () => {
+  test.each([
+    {
+      bbox: new BBox(
+        CRS_EPSG3857,
+        112.81332057952881,
+        63.97041521013803,
+        119.85694837570192,
+        65.98227733565385,
+      ),
+      expected: new BBox(
+        CRS_EPSG3857,
+        112.81332057952881,
+        63.97041521013803,
+        119.85694837570192,
+        65.98227733565385,
+      ),
+    },
+    {
+      bbox: new BBox(
+        CRS_EPSG4326,
+        -114.27429199218751,
+        45.85176048817254,
+        -112.17864990234376,
+        48.21003212234042,
+      ),
+      expected: new BBox(
+        CRS_EPSG3857,
+        -12720955.995332174,
+        5756625.474213193,
+        -12487670.185005816,
+        6141868.096770483,
+      ),
+    },
+  ])('ensureMercatorBBox %p', ({ bbox, expected }) => {
+    expect(ensureMercatorBBox(bbox)).toMatchObject(expected);
+  });
+});
+
+describe('metersPerPixel', () => {
+  test.each([
+    {
+      bbox: new BBox(
+        CRS_EPSG3857,
+        112.81332057952881,
+        63.97041521013803,
+        119.85694837570192,
+        65.98227733565385,
+      ),
+      width: 512,
+      expected: 0.01375708553868674,
+    },
+    {
+      bbox: new BBox(
+        CRS_EPSG3857,
+        -15028131.257091936,
+        2504688.542848655,
+        -12523442.714243278,
+        5009377.085697314,
+      ),
+      width: 512,
+      expected: 4150.788658570558,
+    },
+    {
+      bbox: new BBox(
+        CRS_EPSG3857,
+        -12601714.2312073,
+        5870363.772301538,
+        -12523442.714243278,
+        5948635.289265559,
+      ),
+      width: 512,
+      expected: 104.64937737265413,
+    },
+    {
+      bbox: new BBox(
+        CRS_EPSG4326,
+        -114.27429199218751,
+        45.85176048817254,
+        -112.17864990234376,
+        48.21003212234042,
+      ),
+      width: 512,
+      expected: 310.4876808881625,
+    },
+  ])('metersPerPixel %p', ({ bbox, width, expected }) => {
+    expect(metersPerPixel(bbox, width)).toEqual(expected);
   });
 });
