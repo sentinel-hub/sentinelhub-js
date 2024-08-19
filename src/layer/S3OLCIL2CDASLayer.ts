ea888dc86a30adae8d392e8d37420e40c4370f9c
Zan Pecovnik <zan.pecovnik@sinergise.com>
2024-08-19 14:48:38 +0200

    Extend layer with CC

--- src/layer/S3OLCIL2CDASLayer.ts
+++ src/layer/S3OLCIL2CDASLayer.ts
@@ -1,6 +1,6 @@
 import { DATASET_CDAS_S3OLCIL2 } from './dataset';
-import { S3OLCILayer } from './S3OLCILayer';
+import { S3SLSTRCDASLayer } from './S3SLSTRCDASLayer';
 
-export class S3OLCIL2CDASLayer extends S3OLCILayer {
+export class S3OLCIL2CDASLayer extends S3SLSTRCDASLayer {
   public readonly dataset = DATASET_CDAS_S3OLCIL2;
 }
