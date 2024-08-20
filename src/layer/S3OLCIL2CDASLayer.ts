d9857963c1e6e47d0780885ecddb7bc37790936e
Zan Pecovnik <zan.pecovnik@sinergise.com>
2024-08-20 10:57:19 +0200

    Rather extend new layer with abstract layer with CC instead of S3SLSTR

--- src/layer/S3OLCIL2CDASLayer.ts
+++ src/layer/S3OLCIL2CDASLayer.ts
@@ -1,10 +1,74 @@
+import moment from 'moment';
+
 import { DATASET_CDAS_S3OLCIL2 } from './dataset';
-import { S3SLSTRCDASLayer } from './S3SLSTRCDASLayer';
+import { AbstractSentinelHubV3WithCCLayer } from './AbstractSentinelHubV3WithCCLayer';
+import { RequestConfiguration } from '../utils/cancelRequests';
 
-import { Link, LinkType } from './const';
+import { PaginatedTiles, Link, LinkType, FindTilesAdditionalParameters } from './const';
 
-export class S3OLCIL2CDASLayer extends S3SLSTRCDASLayer {
+type S3OLCIL2FindTilesDatasetParameters = {
+  type?: string;
+};
+
+export class S3OLCIL2CDASLayer extends AbstractSentinelHubV3WithCCLayer {
   public readonly dataset = DATASET_CDAS_S3OLCIL2;
+
+  protected convertResponseFromSearchIndex(response: {
+    data: { tiles: any[]; hasMore: boolean };
+  }): PaginatedTiles {
+    return {
+      tiles: response.data.tiles.map(tile => ({
+        geometry: tile.dataGeometry,
+        sensingTime: moment.utc(tile.sensingTime).toDate(),
+        meta: this.extractFindTilesMeta(tile),
+        links: this.getTileLinks(tile),
+      })),
+      hasMore: response.data.hasMore,
+    };
+  }
+
+  protected getFindTilesAdditionalParameters(): FindTilesAdditionalParameters {
+    const findTilesDatasetParameters: S3OLCIL2FindTilesDatasetParameters = {
+      type: this.dataset.shProcessingApiDatasourceAbbreviation,
+    };
+
+    return {
+      maxCloudCoverPercent: this.maxCloudCoverPercent,
+      datasetParameters: findTilesDatasetParameters,
+    };
+  }
+
+  protected async getFindDatesUTCAdditionalParameters(
+    reqConfig: RequestConfiguration, // eslint-disable-line @typescript-eslint/no-unused-vars
+  ): Promise<Record<string, any>> {
+    const result: Record<string, any> = {
+      datasetParameters: {
+        type: this.dataset.datasetParametersType,
+      },
+    };
+
+    if (this.maxCloudCoverPercent !== null) {
+      result.maxCloudCoverage = this.maxCloudCoverPercent / 100;
+    }
+
+    return result;
+  }
+
+  protected getTileLinks(tile: Record<string, any>): Link[] {
+    return [
+      {
+        target: tile.originalId.replace('EODATA', '/eodata'),
+        type: LinkType.CREODIAS,
+      },
+      {
+        target: `https://finder.creodias.eu/files${tile.originalId.replace(
+          'EODATA',
+          '',
+        )}/${tile.productName.replace('.SEN3', '')}-ql.jpg`,
+        type: LinkType.PREVIEW,
+      },
+    ];
+  }
 
   protected getTileLinksFromCatalog(feature: Record<string, any>): Link[] {
     const { assets } = feature;
