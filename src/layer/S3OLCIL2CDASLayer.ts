ed0efa68ef413d8d00574d07fe1f53ccfb45c05c
Zan Pecovnik <zan.pecovnik@sinergise.com>
2024-08-20 09:02:04 +0200

    Update and use correct getTileLinksFromCatalog function

--- src/layer/S3OLCIL2CDASLayer.ts
+++ src/layer/S3OLCIL2CDASLayer.ts
@@ -1,6 +1,18 @@
 import { DATASET_CDAS_S3OLCIL2 } from './dataset';
 import { S3SLSTRCDASLayer } from './S3SLSTRCDASLayer';
 
+import { Link, LinkType } from './const';
+
 export class S3OLCIL2CDASLayer extends S3SLSTRCDASLayer {
   public readonly dataset = DATASET_CDAS_S3OLCIL2;
+
+  protected getTileLinksFromCatalog(feature: Record<string, any>): Link[] {
+    const { assets } = feature;
+    let result: Link[] = super.getTileLinksFromCatalog(feature);
+
+    if (assets.data && assets.data.href) {
+      result.push({ target: assets.data.href.replace('s3://DIAS', '/dias'), type: LinkType.CREODIAS });
+    }
+    return result;
+  }
 }
