0f91cc002fc998932dde9a4ae99310f6bdf97a3a
Ziga Cernigoj <ziga.cernigoj@planet.com>
2025-08-05 16:15:01 +0200

    remove tests for findTiles and findDatesUTC using searchIndex

--- src/layer/__tests__/S1GRDAWSLayer.ts
+++ src/layer/__tests__/S1GRDAWSLayer.ts
@@ -6,15 +6,11 @@ import {
   Polarization,
   Resolution,
   OrbitDirection,
-  LinkType,
   setAuthToken,
   MosaickingOrder,
 } from '../../index';
 
-import {
-  constructFixtureFindTilesSearchIndex,
-  constructFixtureFindTilesCatalog,
-} from './fixtures.S1GRDAWSLayer';
+import { constructFixtureFindTilesCatalog } from './fixtures.S1GRDAWSLayer';
 
 import {
   AUTH_TOKEN,
@@ -30,11 +26,7 @@ import {
   checkResponseFindDatesUTC,
 } from './testUtils.findDatesUTC';
 
-import {
-  constructFixtureFindDatesUTCSearchIndex,
-  constructFixtureFindDatesUTCCatalog,
-} from './fixtures.findDatesUTC';
-import { DATASET_AWSEU_S1GRD } from '../dataset';
+import { constructFixtureFindDatesUTCCatalog } from './fixtures.findDatesUTC';
 
 test('timezone should NOT be UTC', () => {
   // We are testing correctness in case of local timezones, so it doesn't make sense to
@@ -67,125 +59,7 @@ test('constructor with mosaickingOrder parameter', () => {
   expect(layer.mosaickingOrder).toEqual(expectedMosaickingOrder);
 });
 
-test.each([
-  [true, '2018-11-28T11:12:13Z', new Date(Date.UTC(2018, 11 - 1, 28, 11, 12, 13))],
-  [false, '2018-11-28T11:12:13Z', new Date(Date.UTC(2018, 11 - 1, 28, 11, 12, 13))],
-  [false, '2018-11-11T00:01:02Z', new Date(Date.UTC(2018, 11 - 1, 11, 0, 1, 2))],
-])(
-  'S1GRDLayer.findTiles returns correct data (%p, %p, %p)',
-  async (hasMoreFixture, sensingTimeFixture, expectedSensingTimeFixture) => {
-    const fromTime = new Date(Date.UTC(2018, 11 - 1, 22, 0, 0, 0));
-    const toTime = new Date(Date.UTC(2018, 12 - 1, 22, 23, 59, 59));
-    const bbox = new BBox(CRS_EPSG4326, 19, 20, 20, 21);
-    const layer = new S1GRDAWSEULayer({
-      instanceId: 'INSTANCE_ID',
-      layerId: 'LAYER_ID',
-      acquisitionMode: AcquisitionMode.IW,
-      polarization: Polarization.DV,
-      resolution: Resolution.HIGH,
-    });
-
-    // mock a single-tile response:
-    mockNetwork.reset();
-    mockNetwork.onPost().replyOnce(200, {
-      tiles: [
-        {
-          type: 'S1',
-          id: 1293846,
-          originalId: 'S1A_EW_GRDM_1SDH_20200202T180532_20200202T180632_031077_03921C_E6C8',
-          dataUri:
-            's3://sentinel-s1-l1c/GRD/2020/2/2/EW/DH/S1A_EW_GRDM_1SDH_20200202T180532_20200202T180632_031077_03921C_E6C8',
-          dataGeometry: {
-            type: 'MultiPolygon',
-            crs: {
-              type: 'name',
-              properties: {
-                name: 'urn:ogc:def:crs:EPSG::4326',
-              },
-            },
-            coordinates: [
-              [
-                [
-                  [-28.958387727765576, 77.22089053106154],
-                  [-28.454271377131395, 77.28385150034897],
-                  [-27.718918346651687, 77.37243188785827],
-                  [-26.974008583323926, 77.45890918854761],
-                  [-26.217031402559755, 77.54352656462356],
-                  [-25.447186512415197, 77.62630504330521],
-                  [-24.667542862300945, 77.7068623880844],
-                  [-28.958387727765576, 77.22089053106154],
-                ],
-              ],
-            ],
-          },
-          sensingTime: sensingTimeFixture,
-          rasterWidth: 10459,
-          rasterHeight: 9992,
-          polarization: 'DV',
-          resolution: 'HIGH',
-          orbitDirection: 'ASCENDING',
-          acquisitionMode: 'IW',
-          timeliness: 'NRT3h',
-          additionalData: {},
-          missionDatatakeId: 234012,
-          sliceNumber: 5,
-        },
-      ],
-      hasMore: hasMoreFixture,
-      maxOrderKey: '2020-02-02T08:17:57Z;1295159',
-    });
-
-    const { tiles, hasMore } = await layer.findTiles(bbox, fromTime, toTime, 5, 0);
-
-    expect(mockNetwork.history.post.length).toBe(1);
-    expect(hasMore).toBe(hasMoreFixture);
-    expect(tiles).toStrictEqual([
-      {
-        geometry: {
-          type: 'MultiPolygon',
-          crs: {
-            type: 'name',
-            properties: {
-              name: 'urn:ogc:def:crs:EPSG::4326',
-            },
-          },
-          coordinates: [
-            [
-              [
-                [-28.958387727765576, 77.22089053106154],
-                [-28.454271377131395, 77.28385150034897],
-                [-27.718918346651687, 77.37243188785827],
-                [-26.974008583323926, 77.45890918854761],
-                [-26.217031402559755, 77.54352656462356],
-                [-25.447186512415197, 77.62630504330521],
-                [-24.667542862300945, 77.7068623880844],
-                [-28.958387727765576, 77.22089053106154],
-              ],
-            ],
-          ],
-        },
-        sensingTime: expectedSensingTimeFixture,
-        meta: {
-          acquisitionMode: AcquisitionMode.IW,
-          polarization: Polarization.DV,
-          resolution: Resolution.HIGH,
-          orbitDirection: OrbitDirection.ASCENDING,
-          tileId: 1293846,
-        },
-        links: [
-          {
-            target:
-              's3://sentinel-s1-l1c/GRD/2020/2/2/EW/DH/S1A_EW_GRDM_1SDH_20200202T180532_20200202T180632_031077_03921C_E6C8',
-            type: LinkType.AWS,
-          },
-        ],
-      },
-    ]);
-  },
-);
-
 const CATALOG_URL = 'https://services.sentinel-hub.com/api/v1/catalog/1.0.0/search';
-const SEARCH_INDEX_URL = 'https://services.sentinel-hub.com/index/v3/collections/S1GRD/searchIndex';
 
 const fromTime: Date = new Date(Date.UTC(2020, 4 - 1, 1, 0, 0, 0, 0));
 const toTime: Date = new Date(Date.UTC(2020, 5 - 1, 1, 23, 59, 59, 999));
@@ -230,26 +104,6 @@ const layerParamsArr: Record<string, any>[] = [
   })),
 ];
 
-describe('Test findTiles using searchIndex', () => {
-  beforeEach(async () => {
-    setAuthToken(null);
-    mockNetwork.reset();
-  });
-
-  test('searchIndex is used if token is not set', async () => {
-    await checkIfCorrectEndpointIsUsed(null, constructFixtureFindTilesSearchIndex({}), SEARCH_INDEX_URL);
-  });
-
-  test.each(layerParamsArr)('check if correct request is constructed', async (layerParams) => {
-    const fixtures = constructFixtureFindTilesSearchIndex(layerParams);
-    await checkRequestFindTiles(fixtures);
-  });
-
-  test('response from searchIndex', async () => {
-    await checkResponseFindTiles(constructFixtureFindTilesSearchIndex({}));
-  });
-});
-
 describe('Test findTiles using catalog', () => {
   beforeEach(async () => {
     setAuthToken(AUTH_TOKEN);
@@ -270,71 +124,6 @@ describe('Test findTiles using catalog', () => {
   });
 });
 
-describe('Test findDatesUTC using searchIndex', () => {
-  beforeEach(async () => {
-    setAuthToken(null);
-    mockNetwork.reset();
-  });
-
-  test('findAvailableData is used if token is not set', async () => {
-    const layer = new S1GRDAWSEULayer({
-      instanceId: 'INSTANCE_ID',
-      layerId: 'LAYER_ID',
-    });
-    await checkIfCorrectEndpointIsUsedFindDatesUTC(
-      null,
-      constructFixtureFindDatesUTCSearchIndex(layer, {}),
-      DATASET_AWSEU_S1GRD.findDatesUTCUrl,
-    );
-  });
-
-  test.each(layerParamsArr)('check if correct request is constructed', async (layerParams) => {
-    let constructorParams: Record<string, any> = {};
-
-    if (layerParams && layerParams.acquisitionMode) {
-      constructorParams.acquisitionMode = layerParams.acquisitionMode;
-    }
-
-    if (layerParams && layerParams.orbitDirection) {
-      constructorParams.orbitDirection = layerParams.orbitDirection;
-    }
-
-    if (layerParams && layerParams.polarization) {
-      constructorParams.polarization = layerParams.polarization;
-    }
-
-    if (layerParams && layerParams.resolution) {
-      constructorParams.resolution = layerParams.resolution;
-    }
-
-    const layer = new S1GRDAWSEULayer({
-      instanceId: 'INSTANCE_ID',
-      layerId: 'LAYER_ID',
-      ...constructorParams,
-    });
-    const fixtures = constructFixtureFindDatesUTCSearchIndex(layer, layerParams);
-    await checkRequestFindDatesUTC(fixtures);
-  });
-
-  test('response from service', async () => {
-    const layer = new S1GRDAWSEULayer({
-      instanceId: 'INSTANCE_ID',
-      layerId: 'LAYER_ID',
-      acquisitionMode: AcquisitionMode.IW,
-      polarization: Polarization.DV,
-      resolution: Resolution.HIGH,
-      orbitDirection: OrbitDirection.ASCENDING,
-    });
-    await checkResponseFindDatesUTC(
-      constructFixtureFindDatesUTCSearchIndex(layer, {
-        acquisitionMode: AcquisitionMode.IW,
-        polarization: Polarization.DV,
-        resolution: Resolution.HIGH,
-        orbitDirection: OrbitDirection.ASCENDING,
-      }),
-    );
-  });
-});
 describe('Test findDatesUTC using catalog', () => {
   beforeEach(async () => {
     setAuthToken(AUTH_TOKEN);
